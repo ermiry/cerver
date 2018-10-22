7ea19bc4f953b3a6de7d80f844aafaaa2e9974a8
ermiry <stumpestro@ermiry.com>
2018-10-22 00:48:32 -0500

    Thinking on a load balancer
    Thinking on how to implement a load balancer in the future.

--- include/cerver.h
+++ include/cerver.h
@@ -24,8 +24,6 @@ typedef int64_t i64;
 
 typedef unsigned char asciiChar;
 
-typedef void (*Func)(void *);   // 18/10/2018 -- testing a type of func like in c#
-
 /*** SEVER ***/
 
 #define DEFAULT_PROTOCOL                IPPROTO_TCP
@@ -107,6 +105,33 @@ extern void listenForConnections (Server *);
 extern void cerver_shutdownServer (Server *);
 extern u8 cerver_teardown (Server *);
 extern Server *cerver_restartServer (Server *);
+
+/*** LOAD BALANCER ***/
+
+// this is the generic load balancer struct, used to configure a load balancer
+typedef struct LoadBalancer {
+
+    i32 lbsock;             // lb socket
+    u8 useIpv6;  
+    u8 protocol;            // 12/10/2018 - we only support either tcp or udp
+    u16 port; 
+    u16 connectionQueue;    // number of queue connections
+
+    bool isRunning;         // 22/10/2018 - the lb is handling the traffic
+
+    List *servers;          // list of servers managed by the load balancer
+
+    // Pool *clientPool;       // does the load balancer handles clients directly??
+
+    // 20/10/2018 -- i dont like this...
+    // Vector holdClients;     // hold on the clients until they authenticate
+
+    void (*destroyLoadBalancer) (void *data);   // ptr to a custom functipn to destroy the lb?
+
+    // TODO: 14/10/2018 - maybe we can have listen and handle connections as generic functions, also a generic function
+    // to recieve packets and specific functions to cast the packet to the type that we need?
+
+} LoadBalancer;
 
 /*** REQUESTS ***/
 
