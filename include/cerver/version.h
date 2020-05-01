f60430aa28ae508ee700b3f744d7574215363a4f
ermiry <erick_salas@ermiry.com>
2020-04-30 23:10:44 -0500

    Refactor version method names

--- include/cerver/version.h
+++ include/cerver/version.h
@@ -1,19 +1,19 @@
 #ifndef _CERVER_VERSION_H_
 #define _CERVER_VERSION_H_
 
-#define VERSION                 "1.2.1"
-#define VERSION_NAME            "Release 1.2.1"
-#define VERSION_DATE			"14/04/2020"
-#define VERSION_TIME			"20:55 CST"
+#define VERSION                 "1.2.2"
+#define VERSION_NAME            "Release 1.2.2"
+#define VERSION_DATE			"30/04/2020"
+#define VERSION_TIME			"23:06 CST"
 #define VERSION_AUTHOR			"Erick Salas"
 
 // print full cerver version information 
-extern void version_print_full (void);
+extern void cerver_version_print_full (void);
 
 // print the version id
-extern void version_print_version_id (void);
+extern void cerver_version_print_version_id (void);
 
 // print the version name
-extern void version_print_version_name (void);
+extern void cerver_version_print_version_name (void);
 
 #endif
--- src/cerver/version.c
+++ src/cerver/version.c
@@ -3,24 +3,24 @@
 #include "cerver/version.h"
 
 // print full erver version information 
-void version_print_full (void) {
+void cerver_version_print_full (void) {
 
-    printf ("\n\nCerver Version: %s\n", VERSION_NAME);
+    printf ("\nCerver Version: %s\n", VERSION_NAME);
     printf ("Release Date & time: %s - %s\n", VERSION_DATE, VERSION_TIME);
     printf ("Author: %s\n\n", VERSION_AUTHOR);
 
 }
 
 // print the version id
-void version_print_version_id (void) {
+void cerver_version_print_version_id (void) {
 
-    printf ("\n\nCerver Version ID: %s\n", VERSION);
+    printf ("\nCerver Version ID: %s\n", VERSION);
 
 }
 
 // print the version name
-void version_print_version_name (void) {
+void cerver_version_print_version_name (void) {
 
-    printf ("\n\nCerver Version: %s\n", VERSION_NAME);
+    printf ("\nCerver Version: %s\n", VERSION_NAME);
 
 }
