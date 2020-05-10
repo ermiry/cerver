e69d68aefbea3dd2dbf12101e231a98ad5b947d7
ermiry <erick_salas@ermiry.com>
2020-05-10 18:08:12 -0500

    Moved bsem methods to threads/common

--- include/cerver/threads/common.h
+++ include/cerver/threads/common.h
@@ -16,4 +16,19 @@ extern bsem *bsem_new (void);
 
 extern void bsem_delete (void *bsem_ptr);
 
+// inits semaphore to 1 or 0
+extern void bsem_init (bsem *bsem_p, int value);
+
+// resets semaphore to 0
+extern void bsem_reset (bsem *bsem_p);
+
+// posts to at least one thread
+extern void bsem_post (bsem *bsem_p);
+
+// posts to all threads
+extern void bsem_post_all (bsem *bsem_p);
+
+// waits on semaphore until semaphore has value 0
+extern void bsem_wait (bsem *bsem_p);
+
 #endif
--- src/cerver/threads/common.c
+++ src/cerver/threads/common.c
@@ -3,12 +3,81 @@
 
 #include "cerver/threads/common.h"
 
+#ifdef CERVER_DEBUG
+#include "cerver/utils/log.h"
+#endif
+
 bsem *bsem_new (void) {
 
-    bsem *binary_sem = (bsem *) malloc (sizeof (bsem));
-    if (binary_sem) memset (binary_sem, 0, sizeof (bsem));
-    return binary_sem;
+    bsem *bsem_p = (bsem *) malloc (sizeof (bsem));
+    if (bsem_p) memset (bsem_p, 0, sizeof (bsem));
+    return bsem_p;
 
 }
 
 void bsem_delete (void *bsem_ptr) { if (bsem_ptr) free (bsem_ptr); }
+
+// inits semaphore to 1 or 0
+void bsem_init (bsem *bsem_p, int value) {
+
+    if (bsem_p) {
+        if (value == 0 || value == 1) {
+            pthread_mutex_init (&(bsem_p->mutex), NULL);
+            pthread_cond_init (&(bsem_p->cond), NULL);
+            bsem_p->v = value;
+        }
+
+        else {
+            #ifdef CERVER_DEBUG
+            cerver_log_error ("bsem_init () - Binary semaphore can take only values 1 or 0");
+            #endif
+        }
+    }
+	
+}
+
+// resets semaphore to 0
+void bsem_reset (bsem *bsem_p) {
+
+    if (bsem_p) bsem_init (bsem_p, 0);
+	
+}
+
+// posts to at least one thread
+void bsem_post (bsem *bsem_p) {
+
+    if (bsem_p) {
+        pthread_mutex_lock (&bsem_p->mutex);
+        bsem_p->v = 1;
+        pthread_cond_signal (&bsem_p->cond);
+        pthread_mutex_unlock (&bsem_p->mutex);
+    }
+	
+}
+
+// posts to all threads
+void bsem_post_all (bsem *bsem_p) {
+
+    if (bsem_p) {
+        pthread_mutex_lock (&bsem_p->mutex);
+        bsem_p->v = 1;
+        pthread_cond_broadcast (&bsem_p->cond);
+        pthread_mutex_unlock (&bsem_p->mutex);
+    }
+	
+}
+
+// waits on semaphore until semaphore has value 0
+void bsem_wait (bsem *bsem_p) {
+
+    if (bsem_p) {
+        pthread_mutex_lock (&bsem_p->mutex);
+        while (bsem_p->v != 1) {
+            pthread_cond_wait (&bsem_p->cond, &bsem_p->mutex);
+        }
+
+        bsem_p->v = 0;
+        pthread_mutex_unlock (&bsem_p->mutex);
+    }
+	
+}
