d20566cb8e1a9903e90084be144172d1793e3eeb
ermiry <erick_salas@ermiry.com>
2020-05-11 18:33:15 -0500

    Added script to download an install latest cerver

---
+++ cerver-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# sudo apt-get update && sudo apt-get install -y make wget
+
+cd /opt
+sudo wget -O cerver.tar.gz https://github.com/ermiry/cerver/archive/1.3.tar.gz
+sudo tar xzf cerver.tar.gz
+
+cd /opt/cerver-1.3
+sudo make CC=gcc -j4 && sudo make install && sudo ldconfig
