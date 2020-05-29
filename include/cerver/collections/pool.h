29b0bf6c533d1e5a68e9db23ceca7ecc7462f46b
ermiry <erick_salas@ermiry.com>
2020-05-29 09:19:48 -0500

    Added more methods to pool structure

--- include/cerver/collections/pool.h
+++ include/cerver/collections/pool.h
@@ -12,8 +12,26 @@ typedef struct Pool {
 
 } Pool;
 
+// returns how many elements are inside the pool
+extern size_t pool_size (Pool *pool);
+
 // creates a new pool
-extern Pool *pool_init (void (*destroy)(void *data));
+extern Pool *pool_create (void (*destroy)(void *data));
+
+// uses the create method to populate the pool with n elements
+// returns 0 on no error, 1 if at least one element failed to be inserted
+extern int pool_init (Pool *pool, 
+    void *(*create)(void), unsigned int n_elements);
+
+// deletes the pool and all of its members using the destroy method
+extern void pool_delete (Pool *pool);
+
+// destroys all of the pool's elements and their data but keeps the pool
+extern void pool_reset (Pool *pool);
+
+// only gets rid of the pool's elements, but the data is kept
+// this is usefull if another structure points to the same data
+extern void pool_clear (Pool *pool);
 
 // inserts the new data at the end of the pool
 // returns 0 on success, 1 on error
@@ -21,8 +39,5 @@ extern int pool_push (Pool *pool, void *data);
 
 // returns the data that is first in the pool
 extern void *pool_pop (Pool *pool);
-
-// deletes the pool and all of its members using the destroy method
-extern void pool_delete (Pool *pool);
 
 #endif
--- src/cerver/collections/pool.c
+++ src/cerver/collections/pool.c
@@ -19,7 +19,20 @@ static Pool *pool_new (void) {
 
 #pragma endregion
 
-Pool *pool_init (void (*destroy)(void *data)) {
+// returns how many elements are inside the pool
+size_t pool_size (Pool *pool) {
+
+    size_t retval = 0;
+
+    if (pool) {
+        retval = dlist_size (pool->dlist);
+    }
+
+    return retval;
+
+}
+
+Pool *pool_create (void (*destroy)(void *data)) {
 
     Pool *pool = pool_new ();
     if (pool) {
@@ -31,12 +44,50 @@ Pool *pool_init (void (*destroy)(void *data)) {
 
 }
 
+// uses the create method to populate the pool with n elements
+// returns 0 on no error, 1 if at least one element failed to be inserted
+int pool_init (Pool *pool, 
+    void *(*create)(void), unsigned int n_elements) {
+
+    int errors = 0;
+
+    for (unsigned int i = 0; i < n_elements; i++) {
+        errors |= dlist_insert_after (
+            pool->dlist,
+            dlist_end (pool->dlist),
+            create ()
+        );
+    }
+
+    return errors;
+
+}
+
 void pool_delete (Pool *pool) {
 
     if (pool) {
         dlist_delete (pool->dlist);
 
         free (pool);
+    }
+
+}
+
+// destroys all of the dlist's elements and their data but keeps the dlist
+void pool_reset (Pool *pool) {
+
+    if (pool) {
+        dlist_reset (pool->dlist);
+    }
+
+}
+
+// only gets rid of the list elements, but the data is kept
+// this is usefull if another dlist or structure points to the same data
+void pool_clear (Pool *pool) {
+
+    if (pool) {
+        dlist_clear (pool->dlist);
     }
 
 }
