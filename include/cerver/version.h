db4f30b7641d9abf93f791043877e8ed4682f889
ermiry <erick_salas@ermiry.com>
2020-06-19 05:56:04 -0500

    Updated header version to 1.4.6

--- include/cerver/version.h
+++ include/cerver/version.h
@@ -1,10 +1,10 @@
 #ifndef _CERVER_VERSION_H_
 #define _CERVER_VERSION_H_
 
-#define CERVER_VERSION                  "1.4.5"
-#define CERVER_VERSION_NAME             "Release 1.4.5"
-#define CERVER_VERSION_DATE			    "18/06/2020"
-#define CERVER_VERSION_TIME			    "03:08 CST"
+#define CERVER_VERSION                  "1.4.6"
+#define CERVER_VERSION_NAME             "Release 1.4.6"
+#define CERVER_VERSION_DATE			    "19/06/2020"
+#define CERVER_VERSION_TIME			    "05:52 CST"
 #define CERVER_VERSION_AUTHOR			"Erick Salas"
 
 // print full cerver version information 
