0982898bb6a6d0899b767177d43cc258481695f2
ermiry <erick_salas@ermiry.com>
2020-07-10 02:32:24 -0500

    Updated version to 1.5rc-7

--- include/cerver/version.h
+++ include/cerver/version.h
@@ -1,10 +1,10 @@
 #ifndef _CERVER_VERSION_H_
 #define _CERVER_VERSION_H_
 
-#define CERVER_VERSION                  "1.5rc-6"
-#define CERVER_VERSION_NAME             "Release 1.5rc-6"
-#define CERVER_VERSION_DATE			    "07/07/2020"
-#define CERVER_VERSION_TIME			    "20:33 CST"
+#define CERVER_VERSION                  "1.5rc-7"
+#define CERVER_VERSION_NAME             "Release 1.5rc-7"
+#define CERVER_VERSION_DATE			    "10/07/2020"
+#define CERVER_VERSION_TIME			    "02:32 CST"
 #define CERVER_VERSION_AUTHOR			"Erick Salas"
 
 // print full cerver version information 
