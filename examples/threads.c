892c0a121504cdcc3806127366c2f016b8b1cfe3
ermiry <erick_salas@ermiry.com>
2020-07-25 22:38:18 -0500

    Refactor event methods in threads examples

--- examples/threads.c
+++ examples/threads.c
@@ -86,12 +86,18 @@ static void on_client_connected (void *event_data_ptr) {
 	if (event_data_ptr) {
 		CerverEventData *event_data = (CerverEventData *) event_data_ptr;
 
-		printf (
-			"\nClient %ld connected with sock fd %d to cerver %s!\n\n",
+		char *status = c_string_create (
+			"Client %ld connected with sock fd %d to cerver %s!\n\n",
 			event_data->client->id,
 			event_data->connection->socket->sock_fd, 
 			event_data->cerver->info->name->str
 		);
+
+		if (status) {
+			printf ("\n");
+			cerver_log_msg (stdout, LOG_EVENT, LOG_CLIENT, status);
+			free (status);
+		}
 	}
 
 }
@@ -101,10 +107,16 @@ static void on_client_close_connection (void *event_data_ptr) {
 	if (event_data_ptr) {
 		CerverEventData *event_data = (CerverEventData *) event_data_ptr;
 
-		printf (
-			"\nA client closed a connection to cerver %s!\n\n",
+		char *status = c_string_create (
+			"A client closed a connection to cerver %s!\n",
 			event_data->cerver->info->name->str
 		);
+
+		if (status) {
+			printf ("\n");
+			cerver_log_msg (stdout, LOG_EVENT, LOG_CLIENT, status);
+			free (status);
+		}
 	}
 
 }
