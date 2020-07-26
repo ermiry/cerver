094d108e629d5b1e52944baf8a25dcd9d4e82940
ermiry <erick_salas@ermiry.com>
2020-07-25 21:09:11 -0500

    Fixed release number - Release 1.5

--- include/cerver/version.h
+++ include/cerver/version.h
@@ -1,8 +1,8 @@
 #ifndef _CERVER_VERSION_H_
 #define _CERVER_VERSION_H_
 
-#define CERVER_VERSION                  "1.5rc"
-#define CERVER_VERSION_NAME             "Release 1.5rc"
+#define CERVER_VERSION                  "1.5"
+#define CERVER_VERSION_NAME             "Release 1.5"
 #define CERVER_VERSION_DATE			    "25/07/2020"
 #define CERVER_VERSION_TIME			    "17:46 CST"
 #define CERVER_VERSION_AUTHOR			"Erick Salas"
