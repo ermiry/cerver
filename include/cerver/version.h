3608ba9f6f00ba364af99d819184f36a63f9bc6e
ermiry <erick_salas@ermiry.com>
2020-07-28 14:11:58 -0500

    Updated version time - keeping 1.6rc-1

--- include/cerver/version.h
+++ include/cerver/version.h
@@ -3,8 +3,8 @@
 
 #define CERVER_VERSION                  "1.6rc-1"
 #define CERVER_VERSION_NAME             "Release 1.6rc-1"
-#define CERVER_VERSION_DATE			    "27/07/2020"
-#define CERVER_VERSION_TIME			    "23:45 CST"
+#define CERVER_VERSION_DATE			    "28/07/2020"
+#define CERVER_VERSION_TIME			    "14:11 CST"
 #define CERVER_VERSION_AUTHOR			"Erick Salas"
 
 // print full cerver version information 
