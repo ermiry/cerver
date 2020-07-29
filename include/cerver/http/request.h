1d1dfa08169acf7ba818e1d32c7fd5c540b5afca
ermiry <erick_salas@ermiry.com>
2020-07-28 23:23:51 -0500

    Refactor http request source with new enums & structure

--- include/cerver/http/request.h
+++ include/cerver/http/request.h
@@ -1,16 +1,62 @@
 #ifndef _CERVER_HTTP_REQUEST_H_
 #define _CERVER_HTTP_REQUEST_H_
 
-#include "cerver/types/estring.h"
+typedef enum RequestMethod {
 
-#include "cerver/collections/htab.h"
+	REQUEST_METHOD_DELETE								= 0,
+	REQUEST_METHOD_GET									= 1,
+	REQUEST_METHOD_HEAD									= 2,
+	REQUEST_METHOD_POST									= 3,
+	REQUEST_METHOD_PUT									= 4,
+
+} RequestMethod;
+
+typedef enum RequestHeader {
+
+	REQUEST_HEADER_ACCEPT								= 0,
+	REQUEST_HEADER_ACCEPT_CHARSET						= 1,
+	REQUEST_HEADER_ACCEPT_ENCODING						= 2,
+	REQUEST_HEADER_ACCEPT_LANGUAGE						= 3,
+
+	REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS		= 4,
+
+	REQUEST_HEADER_AUTHORIZATION						= 5,
+
+	REQUEST_HEADER_CACHE_CONTROL						= 6,
+
+	REQUEST_HEADER_CONNECTION							= 7,
+
+	REQUEST_HEADER_CONTENT_LENGTH						= 8,
+	REQUEST_HEADER_CONTENT_TYPE							= 9,
+
+	REQUEST_HEADER_COOKIE								= 10,
+
+	REQUEST_HEADER_DATE									= 11,
+
+	REQUEST_HEADER_EXPECT								= 12,
+
+	REQUEST_HEADER_HOST									= 13,
+
+	REQUEST_HEADER_PROXY_AUTHORIZATION					= 14,
+
+	REQUEST_HEADER_USER_AGENT							= 15,
+
+} RequestHeader;
+
+#define REQUEST_HEADERS_SIZE			16
+#define REQUEST_HEADER_INVALID			16
 
 typedef struct HttpRequest {
 
 	estring *url;
-	Htab *headers;
-	estring *body;  
+	RequestHeader next_header;
+	estring *headers[REQUEST_HEADERS_SIZE];
+	estring *body;
 
 } HttpRequest;
+
+extern HttpRequest *http_request_new (void);
+
+extern void http_request_delete (HttpRequest *http_request);
 
 #endif
--- src/cerver/http/request.c
+++ src/cerver/http/request.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 
+#include "cerver/types/types.h"
 #include "cerver/types/estring.h"
-
-#include "cerver/collections/htab.h"
 
 #include "cerver/http/request.h"
 
@@ -11,7 +10,12 @@ HttpRequest *http_request_new (void) {
 	HttpRequest *http_request = (HttpRequest *) malloc (sizeof (HttpRequest));
 	if (http_request) {
 		http_request->url = NULL;
-		http_request->headers = NULL;
+
+		http_request->next_header = REQUEST_HEADER_INVALID;
+
+		for (u8 i = 0; i < REQUEST_HEADERS_SIZE; i++)
+			http_request->headers[i] = NULL;
+		
 		http_request->body = NULL;
 	}
 
@@ -23,7 +27,10 @@ void http_request_delete (HttpRequest *http_request) {
 
 	if (http_request) {
 		estring_delete (http_request->url);
-		htab_destroy (http_request->headers);
+
+		for (u8 i = 0; i < REQUEST_HEADERS_SIZE; i++)
+			estring_delete (http_request->headers[i]);
+
 		estring_delete (http_request->body);
 
 		free (http_request);
