13a63d1374e6c24b4b7176ae9b83d8c2ec7198c7
ermiry <erick_salas@ermiry.com>
2020-08-05 22:50:24 -0500

    Defined DEPRECATED qualifier to cerver config header

--- include/cerver/config.h
+++ include/cerver/config.h
@@ -5,4 +5,6 @@
 #define CERVER_PRIVATE			extern	// to be used by internal cerver methods
 #define CERVER_PUBLIC 			extern	// used by private methods but can be used by the application
 
+#define DEPRECATED(func) func __attribute__ ((deprecated))
+
 #endif
