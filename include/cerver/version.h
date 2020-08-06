bf55e3aaccd2468831774248856fe4c7df1c99ad
ermiry <erick_salas@ermiry.com>
2020-08-05 23:17:44 -0500

    Updated version to 2.0rc-1

--- include/cerver/version.h
+++ include/cerver/version.h
@@ -3,10 +3,10 @@
 
 #include "cerver/config.h"
 
-#define CERVER_VERSION                  "1.6rc-3"
-#define CERVER_VERSION_NAME             "Release 1.6rc-3"
-#define CERVER_VERSION_DATE			    "30/07/2020"
-#define CERVER_VERSION_TIME			    "22:07 CST"
+#define CERVER_VERSION                  "2.0rc-1"
+#define CERVER_VERSION_NAME             "Release 2.0rc-1"
+#define CERVER_VERSION_DATE			    "05/08/2020"
+#define CERVER_VERSION_TIME			    "23:17 CST"
 #define CERVER_VERSION_AUTHOR			"Erick Salas"
 
 // print full cerver version information 
