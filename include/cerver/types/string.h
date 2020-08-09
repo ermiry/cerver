f03a3a5e08094062b9d9366d7d5b0173e1852d05
ermiry <erick_salas@ermiry.com>
2020-08-08 19:16:34 -0500

    Renamed estring type to String

--- include/cerver/types/string.h
+++ include/cerver/types/string.h
@@ -5,54 +5,54 @@
 
 #include "cerver/config.h"
 
-typedef struct estring {
+typedef struct String {
 
     unsigned int len;
     char *str;
 
-} estring;
+} String;
 
-CERVER_PUBLIC estring *estring_new (const char *str);
+CERVER_PUBLIC String *str_new (const char *str);
 
-CERVER_PUBLIC void estring_delete (void *str_ptr);
+CERVER_PUBLIC void str_delete (void *str_ptr);
 
-CERVER_PUBLIC estring *estring_create (const char *format, ...);
+CERVER_PUBLIC String *str_create (const char *format, ...);
 
-CERVER_PUBLIC int estring_compare (const estring *s1, const estring *s2);
+CERVER_PUBLIC int str_compare (const String *s1, const String *s2);
 
-CERVER_PUBLIC int estring_comparator (const void *a, const void *b);
+CERVER_PUBLIC int str_comparator (const void *a, const void *b);
 
-CERVER_PUBLIC void estring_copy (estring *to, estring *from);
+CERVER_PUBLIC void str_copy (String *to, String *from);
 
-CERVER_PUBLIC void estring_replace (estring *old, const char *str);
+CERVER_PUBLIC void str_replace (String *old, const char *str);
 
 // concatenates two strings into a new one
-CERVER_PUBLIC estring *estring_concat (estring *s1, estring *s2);
+CERVER_PUBLIC String *str_concat (String *s1, String *s2);
 
 // appends a char to the end of the string
 // reallocates the same string
-CERVER_PUBLIC void estring_append_char (estring *s, const char c);
+CERVER_PUBLIC void str_append_char (String *s, const char c);
 
 // appends a c string at the end of the string
 // reallocates the same string
-CERVER_PUBLIC void estring_append_c_string (estring *s, const char *c_str);
+CERVER_PUBLIC void str_append_c_string (String *s, const char *c_str);
 
-CERVER_PUBLIC void estring_to_upper (estring *string);
+CERVER_PUBLIC void str_to_upper (String *string);
 
-CERVER_PUBLIC void estring_to_lower (estring *string);
+CERVER_PUBLIC void str_to_lower (String *string);
 
-CERVER_PUBLIC char **estring_split (estring *string, const char delim, int *n_tokens);
+CERVER_PUBLIC char **str_split (String *string, const char delim, int *n_tokens);
 
-CERVER_PUBLIC void estring_remove_char (estring *string, char garbage);
+CERVER_PUBLIC void str_remove_char (String *string, char garbage);
 
 // removes the last char from a string
-CERVER_PUBLIC void estring_remove_last_char (estring *string);
+CERVER_PUBLIC void str_remove_last_char (String *string);
 
 // check if a str (to_find) is inside str
 // returns 0 on exact match
 // returns 1 if it match the letters but len is different
 // returns -1 if no match
-CERVER_PUBLIC int estring_contains (estring *str, char *to_find);
+CERVER_PUBLIC int str_contains (String *str, char *to_find);
 
 /*** serialization ***/
 
@@ -98,6 +98,6 @@ typedef struct SStringXL {
 } SStringXL;
 
 // returns a ptr to a serialized str
-CERVER_PUBLIC void *estring_selialize (estring *str, SStringSize size);
+CERVER_PUBLIC void *str_selialize (String *str, SStringSize size);
 
 #endif
