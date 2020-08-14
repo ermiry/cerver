11c45aaeb442e18e1cc54a152015b2484f0289cd
ermiry <erick_salas@ermiry.com>
2020-08-13 22:22:18 -0500

    Added base http response headers

--- include/cerver/http/response.h
+++ include/cerver/http/response.h
@@ -2,12 +2,34 @@
 #define _CERVER_HTTP_RESPONSE_H_
 
 #include "cerver/types/types.h"
+#include "cerver/types/string.h"
 
 #include "cerver/http/status.h"
+
+#define RESPONSE_HEADERS_SIZE			4
+
+#define RESPONSE_HEADER_MAP(XX)									\
+	XX(0,  CONTENT_ENCODING,      Content-Encoding)       		\
+	XX(1,  CONTENT_LANGUAGE,      Content-Language)          	\
+	XX(2,  CONTENT_LENGTH,        Content-Length)         		\
+	XX(3,  CONTENT_TYPE,		  Content-Type)         		\
+
+typedef enum ResponseHeader {
+
+	#define XX(num, name, string) RESPONSE_HEADER_##name = num,
+	RESPONSE_HEADER_MAP(XX)
+	#undef XX
+
+} ResponseHeader;
+
+CERVER_PUBLIC const char *http_response_header_str (ResponseHeader header);
 
 typedef struct HttpResponse {
 
 	http_status status;
+
+	u8 n_headers;
+	String *headers[RESPONSE_HEADERS_SIZE];
 
 	void *header;
 	size_t header_len;
@@ -31,6 +53,11 @@ CERVER_EXPORT void http_response_set_status (HttpResponse *res, http_status stat
 // sets the response's header, it will replace the existing one
 // the data will be deleted when the response gets deleted
 CERVER_EXPORT void http_response_set_header (HttpResponse *res, void *header, size_t header_len);
+
+// adds a new header to the response, the headers will be handled when calling 
+// http_response_compile () to generate a continuos header buffer
+// returns 0 on success, 1 on error
+CERVER_EXPORT u8 http_response_add_header (HttpResponse *res, ResponseHeader type, const char *actual_header);
 
 // sets the response's data (body), it will replace the existing one
 // the data will be deleted when the response gets deleted
