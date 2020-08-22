0328c00c890b1f913e7548a1014c95aa397ae284
ermiry <erick_salas@ermiry.com>
2020-08-22 02:40:06 -0500

    Added base client errors source from latest cerver-client

--- include/cerver/client.h
+++ include/cerver/client.h
@@ -92,6 +92,7 @@ struct _Client {
     pthread_mutex_t *lock;
 
     DoubleList *events;
+    DoubleList *errors;
 
     ClientStats *stats;
 
@@ -307,6 +308,80 @@ typedef struct ClientEventData {
 } ClientEventData;
 
 CERVER_PUBLIC void client_event_data_delete (ClientEventData *event_data);
+
+#pragma endregion
+
+#pragma region errors
+
+typedef enum ClientErrorType {
+
+    CLIENT_ERROR_NONE                    = 0,
+
+	CLIENT_ERROR_CERVER_ERROR            = 1, // internal server error, like no memory
+
+	CLIENT_ERROR_FAILED_AUTH             = 2, // we failed to authenticate with the cerver
+
+	CLIENT_ERROR_CREATE_LOBBY            = 3, // failed to create a new game lobby
+	CLIENT_ERROR_JOIN_LOBBY              = 4, // a client / player failed to join an existin lobby
+	CLIENT_ERROR_LEAVE_LOBBY             = 5, // a player failed to leave from a lobby
+	CLIENT_ERROR_FIND_LOBBY              = 6, // failed to find a game lobby for a player
+
+	CLIENT_ERROR_GAME_INIT               = 7, // the game failed to init properly
+	CLIENT_ERROR_GAME_START              = 8, // the game failed to start
+
+} ClientErrorType;
+
+typedef struct ClientError {
+
+	ClientErrorType type;
+	bool create_thread;                 // create a detachable thread to run action
+    bool drop_after_trigger;            // if we only want to trigger the event once
+
+	Action action;                      // the action to be triggered
+    void *action_args;                  // the action arguments
+    Action delete_action_args;          // how to get rid of the data
+
+} ClientError;
+
+// registers an action to be triggered when the specified error occurs
+// if there is an existing action registered to an error, it will be overrided
+// a newly allocated ClientErrorData structure will be passed to your method 
+// that should be free using the client_error_data_delete () method
+// returns 0 on success, 1 on error
+CERVER_EXPORT u8 client_error_register (
+    struct _Client *client, const ClientErrorType error_type,
+	Action action, void *action_args, Action delete_action_args, 
+    bool create_thread, bool drop_after_trigger
+);
+
+// unregisters the action associated with the error types
+// deletes the action args using the delete_action_args () if NOT NULL
+// returns 0 on success, 1 on error
+CERVER_EXPORT u8 client_error_unregister (struct _Client *client, const ClientErrorType error_type);
+
+// triggers all the actions that are registred to an error
+// returns 0 on success, 1 on error
+CERVER_PRIVATE u8 client_error_trigger (
+    const ClientErrorType error_type, 
+	const struct _Client *client, const struct _Connection *connection, 
+	const char *error_message
+);
+
+#pragma region data
+
+// structure that is passed to the user registered method
+typedef struct ClientErrorData {
+
+    const struct _Client *client;
+    const struct _Connection *connection;
+
+    void *action_args;                  // the action arguments set by the user
+
+	String *error_message;
+
+} ClientErrorData;
+
+CERVER_PUBLIC void client_error_data_delete (ClientErrorData *error_data);
 
 #pragma endregion
 
--- src/cerver/client.c
+++ src/cerver/client.c
@@ -144,6 +144,7 @@ Client *client_new (void) {
         client->lock = NULL;
 
         client->events = NULL;
+        client->errors = NULL;
 
         client->stats = NULL;   
     }
@@ -184,6 +185,7 @@ void client_delete (void *ptr) {
         }
 
         dlist_delete (client->events);
+        dlist_delete (client->errors);
 
         client_stats_delete (client->stats);
 
@@ -1158,6 +1160,239 @@ void client_event_trigger (const ClientEventType event_type,
             }
         }
     }
+
+}
+
+#pragma endregion
+
+#pragma region errors
+
+u8 client_error_unregister (Client *client, const ClientErrorType error_type);
+
+static ClientErrorData *client_error_data_new (void) {
+
+	ClientErrorData *error_data = (ClientErrorData *) malloc (sizeof (ClientErrorData));
+	if (error_data) {
+		error_data->client = NULL;
+		error_data->connection = NULL;
+
+		error_data->action_args = NULL;
+
+		error_data->error_message = NULL;
+	}
+
+	return error_data;
+
+}
+
+void client_error_data_delete (ClientErrorData *error_data) {
+
+	if (error_data) {
+		str_delete (error_data->error_message);
+
+		free (error_data);
+	}
+
+}
+
+static ClientErrorData *client_error_data_create (
+	const Client *client, const Connection *connection, 
+	void *args,
+	const char *error_message
+) {
+
+	ClientErrorData *error_data = client_error_data_new ();
+	if (error_data) {
+		error_data->client = client;
+		error_data->connection = connection;
+
+		error_data->action_args = args;
+
+		error_data->error_message = error_message ? str_new (error_message) : NULL;
+	}
+
+	return error_data;
+
+}
+
+static ClientError *client_error_new (void) {
+
+	ClientError *client_error = (ClientError *) malloc (sizeof (ClientError));
+	if (client_error) {
+		client_error->type = CLIENT_ERROR_NONE;
+
+		client_error->create_thread = false;
+		client_error->drop_after_trigger = false;
+
+		client_error->action = NULL;
+		client_error->action_args = NULL;
+		client_error->delete_action_args = NULL;
+	}
+
+	return client_error;
+
+}
+
+static void client_error_delete (void *client_error_ptr) {
+
+	if (client_error_ptr) {
+		ClientError *client_error = (ClientError *) client_error_ptr;
+
+		if (client_error->action_args) {
+			if (client_error->delete_action_args) 
+				client_error->delete_action_args (client_error->action_args);
+		}
+
+		free (client_error_ptr);
+	}
+
+}
+
+static ClientError *client_error_get (
+    const Client *client, const ClientErrorType error_type, 
+    ListElement **le_ptr
+) {
+
+    if (client) {
+        if (client->errors) {
+            ClientError *error = NULL;
+            for (ListElement *le = dlist_start (client->errors); le; le = le->next) {
+                error = (ClientError *) le->data;
+                if (error->type == error_type) {
+                    if (le_ptr) *le_ptr = le;
+                    return error;
+                } 
+            }
+        }
+    }
+
+    return NULL;
+
+}
+
+static void client_error_pop (DoubleList *list, ListElement *le) {
+
+    if (le) {
+        void *data = dlist_remove_element (list, le);
+        if (data) client_error_delete (data);
+    }
+
+}
+
+// registers an action to be triggered when the specified error occurs
+// if there is an existing action registered to an error, it will be overrided
+// a newly allocated ClientErrorData structure will be passed to your method 
+// that should be free using the client_error_data_delete () method
+// returns 0 on success, 1 on error
+u8 client_error_register (
+    Client *client, const ClientErrorType error_type,
+	Action action, void *action_args, Action delete_action_args, 
+    bool create_thread, bool drop_after_trigger
+) {
+
+	u8 retval = 1;
+
+	if (client) {
+		if (client->errors) {
+			ClientError *error = client_error_new ();
+			if (error) {
+				error->type = error_type;
+
+				error->create_thread = create_thread;
+				error->drop_after_trigger = drop_after_trigger;
+
+				error->action = action;
+				error->action_args = action_args;
+				error->delete_action_args = delete_action_args;
+
+				// search if there is an action already registred for that error and remove it
+				(void) client_error_unregister (client, error_type);
+
+				if (!dlist_insert_after (
+					client->errors,
+					dlist_end (client->errors),
+					error
+				)) {
+					retval = 0;
+				}
+			}
+		}
+	}
+
+	return retval;
+
+}
+
+// unregisters the action associated with the error types
+// deletes the action args using the delete_action_args () if NOT NULL
+// returns 0 on success, 1 on error
+u8 client_error_unregister (Client *client, const ClientErrorType error_type) {
+
+	u8 retval = 1;
+
+    if (client) {
+        if (client->errors) {
+            ClientError *error = NULL;
+            for (ListElement *le = dlist_start (client->errors); le; le = le->next) {
+                error = (ClientError *) le->data;
+                if (error->type == error_type) {
+                    client_error_delete (dlist_remove_element (client->errors, le));
+					retval = 0;
+
+                    break;
+                }
+            }
+        }
+    }
+
+	return retval;
+
+}
+
+// triggers all the actions that are registred to an error
+// returns 0 on success, 1 on error
+u8 client_error_trigger (const ClientErrorType error_type, 
+	const Client *client, const Connection *connection, 
+	const char *error_message
+) {
+
+	u8 retval = 1;
+
+    if (client) {
+        ListElement *le = NULL;
+        ClientError *error = client_error_get (client, error_type, &le);
+        if (error) {
+            // trigger the action
+            if (error->action) {
+                if (error->create_thread) {
+                    pthread_t thread_id = 0;
+                    retval = thread_create_detachable (
+                        &thread_id,
+                        (void *(*)(void *)) error->action, 
+                        client_error_data_create (
+                            client, connection,
+                            error,
+							error_message
+                        )
+                    );
+                }
+
+                else {
+                    error->action (client_error_data_create (
+                        client, connection, 
+                        error,
+						error_message
+                    ));
+
+					retval = 0;
+                }
+                
+                if (error->drop_after_trigger) client_error_pop (client->errors, le);
+            }
+        }
+    }
+
+	return retval;
 
 }
 
