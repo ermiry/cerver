d245f2253ad21e8cb5eb76509ceed54ff428c502
ermiry <erick_salas@ermiry.com>
2020-09-12 18:17:02 -0500

    Added balancer_create () & balancer_service_register ()

--- include/cerver/balancer.h
+++ include/cerver/balancer.h
@@ -1,6 +1,8 @@
 #ifndef _CERVER_BALANCER_H_
 #define _CERVER_BALANCER_H_
 
+#include "cerver/types/types.h"
+
 #include "cerver/cerver.h"
 #include "cerver/client.h"
 #include "cerver/connection.h"
@@ -18,6 +20,8 @@ typedef enum BalcancerType {
 
 } BalcancerType;
 
+const char *balancer_type_to_string (BalcancerType type);
+
 typedef struct Balancer {
 
 	BalcancerType type;
@@ -25,6 +29,7 @@ typedef struct Balancer {
 	Cerver *cerver;
 	Client *client;
 
+	unsigned int next_service;
 	unsigned int n_services;		// how many services the load balancer is connected to
 	Connection **services;			// references to the client's connections for direct access
 
@@ -33,5 +38,21 @@ typedef struct Balancer {
 CERVER_PRIVATE Balancer *balancer_new (void);
 
 CERVER_PRIVATE void balancer_delete (void *balancer_ptr);
+
+// create a new load balancer of the selected type
+// set its network values & set the number of services it will handle
+CERVER_EXPORT Balancer *balancer_create (
+	BalcancerType type,
+	u16 port, u16 connection_queue,
+	unsigned int n_services
+);
+
+// registers a new service to the load balancer
+// a dedicated connection will be created when the balancer starts to handle traffic to & from the service
+// returns 0 on success, 1 on error
+CERVER_EXPORT u8 balancer_service_register (
+	Balancer *balancer,
+	const char *ip_address, u16 port
+);
 
 #endif
--- src/cerver/balancer.c
+++ src/cerver/balancer.c
@@ -1,9 +1,25 @@
 #include <stdlib.h>
 
+#include "cerver/types/types.h"
+
 #include "cerver/balancer.h"
 #include "cerver/cerver.h"
 #include "cerver/client.h"
 #include "cerver/connection.h"
+
+const char *balancer_type_to_string (BalcancerType type) {
+
+	switch (type) {
+		#define XX(num, name, string) case BALANCER_TYPE_##name: return #string;
+		BALANCER_TYPE_MAP(XX)
+		#undef XX
+	}
+
+	return balancer_type_to_string (BALANCER_TYPE_NONE);
+
+}
+
+#pragma region main
 
 Balancer *balancer_new (void) {
 
@@ -14,6 +30,7 @@ Balancer *balancer_new (void) {
 		balancer->cerver = NULL;
 		balancer->client = NULL;
 
+		balancer->next_service = 0;
 		balancer->n_services = 0;
 		balancer->services = NULL;
 	}
@@ -38,3 +55,77 @@ void balancer_delete (void *balancer_ptr) {
 	}
 
 }
+
+// create a new load balancer of the selected type
+// set its network values & set the number of services it will handle
+Balancer *balancer_create (
+	BalcancerType type,
+	u16 port, u16 connection_queue,
+	unsigned int n_services
+) {
+
+	Balancer *balancer = balancer_new ();
+	if (balancer) {
+		balancer->type = type;
+
+		balancer->cerver = cerver_create (
+			CERVER_TYPE_BALANCER,
+			"load-balancer-cerver",
+			port, PROTOCOL_TCP, false, connection_queue, DEFAULT_POLL_TIMEOUT
+		);
+
+		balancer->client = client_create ();
+		client_set_name (balancer->client, "balancer-client");
+
+		balancer->n_services = n_services;
+		balancer->services = (Connection **) calloc (balancer->n_services, sizeof (Connection *));
+		if (balancer->services) {
+			for (unsigned int i = 0; i < balancer->n_services; i++)
+				balancer->services[i] = NULL;
+		}
+	}
+
+	return balancer;
+
+}
+
+#pragma endregion
+
+#pragma region services
+
+// registers a new service to the load balancer
+// a dedicated connection will be created when the balancer starts to handle traffic to & from the service
+// returns 0 on success, 1 on error
+u8 balancer_service_register (
+	Balancer *balancer,
+	const char *ip_address, u16 port
+) {
+
+	u8 retval = 1;
+
+	if (balancer && ip_address) {
+		Connection *connection = client_connection_create (
+			balancer->client, 
+			ip_address, port,
+			PROTOCOL_TCP, false 
+		);
+
+		if (connection) {
+			char name[64] = { 0 };
+			snprintf (name, 64, "service-%d", balancer->n_services);
+
+			connection_set_name (connection, name);
+			connection_set_max_sleep (connection, 30);
+
+			balancer->services[balancer->next_service] = connection;
+			balancer->next_service += 1;
+
+			retval = 0;
+		}
+	}
+
+	return retval;
+
+}
+
+#pragma endregion
