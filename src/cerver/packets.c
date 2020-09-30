90b3f52f754f3ede7722331d14d462c8a83b419b
ermiry <erick_salas@ermiry.com>
2020-09-29 20:07:07 -0500

    Added sock fd field in PacketHeader to be used with load balancers

--- src/cerver/packets.c
+++ src/cerver/packets.c
@@ -135,6 +135,8 @@ PacketHeader *packet_header_create (PacketType packet_type, size_t packet_size, 
 		header->handler_id = 0;
 
 		header->request_type = req_type;
+
+		header->sock_fd = 0;
 	}
 
 	return header;
@@ -148,6 +150,7 @@ void packet_header_print (PacketHeader *header) {
 		printf ("Packet size: %ld\n", header->packet_size);
 		printf ("Handler id: %d\n", header->handler_id);
 		printf ("Request type: %d\n", header->request_type);
+		printf ("Sock fd: %d\n", header->sock_fd);
 	}
 
 }
