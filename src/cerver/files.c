d883cc64bff56c30a74c9b6d40ba8278e9bfc507
ermiry <erick_salas@ermiry.com>
2020-10-04 23:12:02 -0500

    Changed files sources indentation from spaces to tabs

--- src/cerver/files.c
+++ src/cerver/files.c
@@ -39,7 +39,7 @@ unsigned int files_create_dir (const char *dir_path, mode_t mode) {
 				else {
 					char *s = c_string_create ("Failed to create dir %s!", dir_path);
 					if (s) {
-                        cerver_log_error (s);
+						cerver_log_error (s);
 						free (s);
 					}
 				}
@@ -47,7 +47,7 @@ unsigned int files_create_dir (const char *dir_path, mode_t mode) {
 			case 0: {
 				char *s = c_string_create ("Dir %s already exists!", dir_path);
 				if (s) {
-                    cerver_log_warning (s);
+					cerver_log_warning (s);
 					free (s);
 				}
 			} break;
@@ -64,156 +64,156 @@ unsigned int files_create_dir (const char *dir_path, mode_t mode) {
 // NULL if no file extension
 char *files_get_file_extension (const char *filename) {
 
-    char *retval = NULL;
+	char *retval = NULL;
 
-    if (filename) {
-        char *ptr = strrchr ((char *) filename, '.');
-        if (ptr) {
-            // *ptr++;
-            size_t ext_len = 0;
-            char *p = ptr;
-            while (*p++) ext_len++;
+	if (filename) {
+		char *ptr = strrchr ((char *) filename, '.');
+		if (ptr) {
+			// *ptr++;
+			size_t ext_len = 0;
+			char *p = ptr;
+			while (*p++) ext_len++;
 
-            if (ext_len) {
-                retval = (char *) calloc (ext_len + 1, sizeof (char));
-                if (retval) {
-                    memcpy (retval, ptr + 1, ext_len);
-                    retval[ext_len] = '\0';
-                }
-            }
-        }
-        
-    }
+			if (ext_len) {
+				retval = (char *) calloc (ext_len + 1, sizeof (char));
+				if (retval) {
+					memcpy (retval, ptr + 1, ext_len);
+					retval[ext_len] = '\0';
+				}
+			}
+		}
 
-    return retval;
+	}
+
+	return retval;
 
 }
 
 // returns a list of strings containg the names of all the files in the directory
 DoubleList *files_get_from_dir (const char *dir) {
 
-    DoubleList *images = NULL;
+	DoubleList *images = NULL;
 
-    if (dir) {
+	if (dir) {
 		images = dlist_init (str_delete, str_comparator);
 
-        DIR *dp = opendir (dir);
-        if (dp) {
-            struct dirent *ep = NULL;
-            String *file = NULL;
-            while ((ep = readdir (dp)) != NULL) {
-                if (strcmp (ep->d_name, ".") && strcmp (ep->d_name, "..")) {
-                    file = str_create ("%s/%s", dir, ep->d_name);
+		DIR *dp = opendir (dir);
+		if (dp) {
+			struct dirent *ep = NULL;
+			String *file = NULL;
+			while ((ep = readdir (dp)) != NULL) {
+				if (strcmp (ep->d_name, ".") && strcmp (ep->d_name, "..")) {
+					file = str_create ("%s/%s", dir, ep->d_name);
 
-                    dlist_insert_after (images, dlist_end (images), file);
-                }
-            }
+					dlist_insert_after (images, dlist_end (images), file);
+				}
+			}
 
-            (void) closedir (dp);
-        }
+			(void) closedir (dp);
+		}
 
-        else {
+		else {
 			char *status = c_string_create ("Failed to open dir %s", dir);
 			if (status) {
 				cerver_log_error (status);
 				free (status);
 			}
-        }
-    }
+		}
+	}
 
-    return images;
+	return images;
 
 }
 
 static String *file_get_line (FILE *file) {
 
-    String *str = NULL;
+	String *str = NULL;
 
-    if (file) {
-        if (!feof (file)) {
-            char line[1024];
-            if (fgets (line, 1024, file)) {
-                size_t curr = strlen(line);
-                if(line[curr - 1] == '\n') line[curr - 1] = '\0';
+	if (file) {
+		if (!feof (file)) {
+			char line[1024];
+			if (fgets (line, 1024, file)) {
+				size_t curr = strlen(line);
+				if(line[curr - 1] == '\n') line[curr - 1] = '\0';
 
-                str = str_new (line);
-            }
-        }
-    }
+				str = str_new (line);
+			}
+		}
+	}
 
-    return str;
+	return str;
 
 }
 
 // reads eachone of the file's lines into a newly created string and returns them inside a dlist
 DoubleList *file_get_lines (const char *filename) {
 
-    DoubleList *lines = NULL;
+	DoubleList *lines = NULL;
 
-    if (filename) {
-        FILE *file = fopen (filename, "r");
-        if (file) {
-            lines = dlist_init (str_delete, str_comparator);
-            
-            String *line = NULL;
-            while ((line = file_get_line (file))) {
-                dlist_insert_after (lines, dlist_end (lines), line);
-            }
+	if (filename) {
+		FILE *file = fopen (filename, "r");
+		if (file) {
+			lines = dlist_init (str_delete, str_comparator);
 
-            fclose (file);
-        }
+			String *line = NULL;
+			while ((line = file_get_line (file))) {
+				dlist_insert_after (lines, dlist_end (lines), line);
+			}
 
-        else {
-            char *status = c_string_create ("Failed to open file: %s", filename);
-            if (status) {
-                cerver_log_error (status);
-                free (status);
-            }
-        }
-    }
+			fclose (file);
+		}
 
-    return lines;
+		else {
+			char *status = c_string_create ("Failed to open file: %s", filename);
+			if (status) {
+				cerver_log_error (status);
+				free (status);
+			}
+		}
+	}
+
+	return lines;
 
 }
 
 // returns true if the filename exists
 bool file_exists (const char *filename) {
 
-    bool retval = false;
+	bool retval = false;
 
-    if (filename) {
-        struct stat filestatus = { 0 };
-        if (!stat (filename, &filestatus)) {
-            retval = true;
-        }
-    }
+	if (filename) {
+		struct stat filestatus = { 0 };
+		if (!stat (filename, &filestatus)) {
+			retval = true;
+		}
+	}
 
-    return retval;
+	return retval;
 
 }
 
 // opens a file and returns it as a FILE
 FILE *file_open_as_file (const char *filename, const char *modes, struct stat *filestatus) {
 
-    FILE *fp = NULL;
+	FILE *fp = NULL;
 
-    if (filename) {
-        memset (filestatus, 0, sizeof (struct stat));
-        if (!stat (filename, filestatus)) 
-            fp = fopen (filename, modes);
+	if (filename) {
+		memset (filestatus, 0, sizeof (struct stat));
+		if (!stat (filename, filestatus))
+			fp = fopen (filename, modes);
 
-        else {
-            #ifdef CERVER_DEBUG
-            char *s = c_string_create ("File %s not found!", filename);
-            if (s) {
-                cerver_log_msg (stderr, LOG_TYPE_ERROR, LOG_TYPE_FILE, s);
-                free (s);
-            }
-            #endif
-        } 
-    }
+		else {
+			#ifdef CERVER_DEBUG
+			char *s = c_string_create ("File %s not found!", filename);
+			if (s) {
+				cerver_log_msg (stderr, LOG_TYPE_ERROR, LOG_TYPE_FILE, s);
+				free (s);
+			}
+			#endif
+		}
+	}
 
-    return fp;
+	return fp;
 
 }
 
@@ -221,43 +221,43 @@ FILE *file_open_as_file (const char *filename, const char *modes, struct stat *f
 // sets file size to the amount of bytes read
 char *file_read (const char *filename, size_t *file_size) {
 
-    char *file_contents = NULL;
+	char *file_contents = NULL;
 
-    if (filename) {
-        struct stat filestatus = { 0 };
-        FILE *fp = file_open_as_file (filename, "rt", &filestatus);
-        if (fp) {
-            *file_size = filestatus.st_size;
-            file_contents = (char *) malloc (filestatus.st_size);
+	if (filename) {
+		struct stat filestatus = { 0 };
+		FILE *fp = file_open_as_file (filename, "rt", &filestatus);
+		if (fp) {
+			*file_size = filestatus.st_size;
+			file_contents = (char *) malloc (filestatus.st_size);
 
-            // read the entire file into the buffer
-            if (fread (file_contents, filestatus.st_size, 1, fp) != 1) {
-                #ifdef CERVER_DEBUG
-                char *s = c_string_create ("Failed to read file (%s) contents!");
-                if (s) {
-                    cerver_log_msg (stderr, LOG_TYPE_ERROR, LOG_TYPE_FILE, s);
-                    free (s);
-                }
-                #endif
+			// read the entire file into the buffer
+			if (fread (file_contents, filestatus.st_size, 1, fp) != 1) {
+				#ifdef CERVER_DEBUG
+				char *s = c_string_create ("Failed to read file (%s) contents!");
+				if (s) {
+					cerver_log_msg (stderr, LOG_TYPE_ERROR, LOG_TYPE_FILE, s);
+					free (s);
+				}
+				#endif
 
-                free (file_contents);
-            }
+				free (file_contents);
+			}
 
-            fclose (fp);
-        }
+			fclose (fp);
+		}
 
-        else {
-            #ifdef CERVER_DEBUG
-            char *s = c_string_create ("Unable to open file %s.", filename);
-            if (s) {
-                cerver_log_msg (stderr, LOG_TYPE_ERROR, LOG_TYPE_FILE, s);
-                free (s);
-            }
-            #endif
-        }
-    }
+		else {
+			#ifdef CERVER_DEBUG
+			char *s = c_string_create ("Unable to open file %s.", filename);
+			if (s) {
+				cerver_log_msg (stderr, LOG_TYPE_ERROR, LOG_TYPE_FILE, s);
+				free (s);
+			}
+			#endif
+		}
+	}
 
-    return file_contents;
+	return file_contents;
 
 }
 
@@ -265,25 +265,25 @@ char *file_read (const char *filename, size_t *file_size) {
 // returns fd on success, -1 on error
 int file_open_as_fd (const char *filename, struct stat *filestatus, int flags) {
 
-    int retval = -1;
+	int retval = -1;
 
-    if (filename) {
-        memset (filestatus, 0, sizeof (struct stat));
-        if (!stat (filename, filestatus)) 
-            retval = open (filename, flags);
+	if (filename) {
+		memset (filestatus, 0, sizeof (struct stat));
+		if (!stat (filename, filestatus))
+			retval = open (filename, flags);
 
-        else {
-            #ifdef CERVER_DEBUG
-            char *s = c_string_create ("File %s not found!", filename);
-            if (s) {
-                cerver_log_msg (stderr, LOG_TYPE_ERROR, LOG_TYPE_FILE, s);
-                free (s);
-            }
-            #endif
-        } 
-    }
+		else {
+			#ifdef CERVER_DEBUG
+			char *s = c_string_create ("File %s not found!", filename);
+			if (s) {
+				cerver_log_msg (stderr, LOG_TYPE_ERROR, LOG_TYPE_FILE, s);
+				free (s);
+			}
+			#endif
+		}
+	}
 
-    return retval;
+	return retval;
 
 }
 
@@ -291,45 +291,45 @@ int file_open_as_fd (const char *filename, struct stat *filestatus, int flags) {
 // returns 0 on success, 1 on error
 int file_send (const char *filename, int sock_fd) {
 
-    int retval = 1;
+	int retval = 1;
 
-    if (filename) {
-        // try to open the file
-        struct stat filestatus;
-        int fd = file_open_as_fd (filename, &filestatus, O_RDONLY);
-        if (fd >= 0) {
-            // send a first packet with file info
-            // TODO:
-        }
+	if (filename) {
+		// try to open the file
+		struct stat filestatus;
+		int fd = file_open_as_fd (filename, &filestatus, O_RDONLY);
+		if (fd >= 0) {
+			// send a first packet with file info
+			// TODO:
+		}
 
-        else {
-            #ifdef CERVER_DEBUG
-            char *s = c_string_create ("Failed to open file %s.", filename);
-            if (s) {
-                cerver_log_msg (stderr, LOG_TYPE_ERROR, LOG_TYPE_FILE, s);
-                free (s);
-            }
-            #endif
-        }        
-    }
+		else {
+			#ifdef CERVER_DEBUG
+			char *s = c_string_create ("Failed to open file %s.", filename);
+			if (s) {
+				cerver_log_msg (stderr, LOG_TYPE_ERROR, LOG_TYPE_FILE, s);
+				free (s);
+			}
+			#endif
+		}
+	}
 
-    return retval;
+	return retval;
 
 }
 
 json_value *file_json_parse (const char *filename) {
 
-    json_value *value = NULL;
+	json_value *value = NULL;
 
-    if (filename) {
-        size_t file_size = 0;
-        char *file_contents = file_read (filename, &file_size);
-        json_char *json = (json_char *) file_contents;
-        value = json_parse (json, file_size);
+	if (filename) {
+		size_t file_size = 0;
+		char *file_contents = file_read (filename, &file_size);
+		json_char *json = (json_char *) file_contents;
+		value = json_parse (json, file_size);
 
-        free (file_contents);
-    }
+		free (file_contents);
+	}
 
-    return value;
-    
+	return value;
+
 }
