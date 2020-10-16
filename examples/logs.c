98c3048ce84d67ae4a98fe75545e6e2c7326f89f
ermiry <erick_salas@ermiry.com>
2020-10-15 23:46:01 -0500

    Refactor logs example

--- examples/logs.c
+++ examples/logs.c
@@ -28,7 +28,9 @@ static void end (int dummy) {
 	if (my_cerver) {
 		cerver_stats_print (my_cerver, true, true);
 		cerver_teardown (my_cerver);
-	} 
+	}
+
+	cerver_end ();
 
 	exit (0);
 
@@ -40,23 +42,21 @@ static void end (int dummy) {
 
 static void handle_test_request (Packet *packet) {
 
-	if (packet) {
-		// cerver_log_debug ("Got a test message from client. Sending another one back...");
-		cerver_log_msg (stdout, LOG_TYPE_DEBUG, LOG_TYPE_NONE, "Got a test message from client. Sending another one back...");
-		
-		Packet *test_packet = packet_generate_request (PACKET_TYPE_APP, TEST_MSG, NULL, 0);
-		if (test_packet) {
-			packet_set_network_values (test_packet, NULL, NULL, packet->connection, NULL);
-			size_t sent = 0;
-			if (packet_send (test_packet, 0, &sent, false)) 
-				cerver_log_error ("Failed to send test packet to client!");
+	// cerver_log_debug ("Got a test message from client. Sending another one back...");
+	cerver_log_msg (stdout, LOG_TYPE_DEBUG, LOG_TYPE_NONE, "Got a test message from client. Sending another one back...");
+	
+	Packet *test_packet = packet_generate_request (PACKET_TYPE_APP, TEST_MSG, NULL, 0);
+	if (test_packet) {
+		packet_set_network_values (test_packet, NULL, NULL, packet->connection, NULL);
+		size_t sent = 0;
+		if (packet_send (test_packet, 0, &sent, false)) 
+			cerver_log_error ("Failed to send test packet to client!");
 
-			else {
-				// printf ("Response packet sent: %ld\n", sent);
-			}
-			
-			packet_delete (test_packet);
+		else {
+			// printf ("Response packet sent: %ld\n", sent);
 		}
+		
+		packet_delete (test_packet);
 	}
 
 }
@@ -80,46 +80,6 @@ static void handler (void *data) {
 #pragma endregion
 
 #pragma region events
-
-static void on_cever_started (void *event_data_ptr) {
-
-	if (event_data_ptr) {
-		CerverEventData *event_data = (CerverEventData *) event_data_ptr;
-
-		char *status = c_string_create (
-			"Cerver %s has started!\n", 
-			event_data->cerver->info->name->str
-		);
-
-		if (status) {
-			printf ("\n");
-			cerver_log_msg (stdout, LOG_TYPE_EVENT, LOG_TYPE_CERVER, status);
-			free (status);
-		}
-
-		printf ("Test Message: %s\n\n", ((String *) event_data->action_args)->str);
-	}
-
-}
-
-static void on_cever_teardown (void *event_data_ptr) {
-
-	if (event_data_ptr) {
-		CerverEventData *event_data = (CerverEventData *) event_data_ptr;
-
-		char *status = c_string_create (
-			"Cerver %s is going to be destroyed!\n", 
-			event_data->cerver->info->name->str
-		);
-
-		if (status) {
-			printf ("\n");
-			cerver_log_msg (stdout, LOG_TYPE_EVENT, LOG_TYPE_CERVER, status);
-			free (status);
-		}
-	}
-
-}
 
 static void on_client_connected (void *event_data_ptr) {
 
@@ -165,38 +125,6 @@ static void on_client_close_connection (void *event_data_ptr) {
 
 #pragma region main
 
-static u16 get_port (int argc, char **argv) {
-
-	u16 port = 7000;
-	if (argc > 1) {
-		int j = 0;
-		const char *curr_arg = NULL;
-		for (int i = 1; i < argc; i++) {
-			curr_arg = argv[i];
-
-			// port
-			if (!strcmp (curr_arg, "-p")) {
-				j = i + 1;
-				if (j <= argc) {
-					port = (u16) atoi (argv[j]);
-					i++;
-				}
-			}
-
-			else {
-				char *status = c_string_create ("Unknown argument: %s", curr_arg);
-				if (status) {
-					cerver_log_warning (status);
-					free (status);
-				}
-			}
-		}
-	}
-
-	return port;
-
-}
-
 int main (int argc, char **argv) {
 
 	srand (time (NULL));
@@ -204,7 +132,8 @@ int main (int argc, char **argv) {
 	// register to the quit signal
 	signal (SIGINT, end);
 
-	printf ("\n");
+	cerver_init ();
+
 	cerver_version_print_full ();
 	printf ("\n");
 
@@ -213,78 +142,52 @@ int main (int argc, char **argv) {
 	cerver_log_debug ("Simple test cerver with custom logs configuartions");
 	printf ("\n");
 
-	char *name = "Erick Salas";
-	int num = 10;
-	cerver_log (LOG_TYPE_SUCCESS, LOG_TYPE_CERVER, "Hola %s yo soy el numero %d", name, num);
-	cerver_log (LOG_TYPE_ERROR, LOG_TYPE_CERVER, "Hola %s yo soy el numero %d", name, num);
-	cerver_log (LOG_TYPE_WARNING, LOG_TYPE_CERVER, "Hola %s yo soy el numero %d", name, num);
-	cerver_log (LOG_TYPE_TEST, LOG_TYPE_CERVER, "Hola %s yo soy el numero %d", name, num);
-	cerver_log (LOG_TYPE_CERVER, LOG_TYPE_CERVER, "Hola %s yo soy el numero %d", name, num);
-	cerver_log (LOG_TYPE_CLIENT, LOG_TYPE_CERVER, "Hola %s yo soy el numero %d", name, num);
-	cerver_log (LOG_TYPE_HANDLER, LOG_TYPE_CERVER, "Hola %s yo soy el numero %d", name, num);
-	cerver_log (LOG_TYPE_ADMIN, LOG_TYPE_CERVER, "Hola %s yo soy el numero %d", name, num);
-	cerver_log (LOG_TYPE_EVENT, LOG_TYPE_CERVER, "Hola %s yo soy el numero %d", name, num);
-	cerver_log (LOG_TYPE_PACKET, LOG_TYPE_CERVER, "Hola %s yo soy el numero %d", name, num);
+	my_cerver = cerver_create (CERVER_TYPE_CUSTOM, "my-cerver", 7000, PROTOCOL_TCP, false, 2, 2000);
+	if (my_cerver) {
+		cerver_set_welcome_msg (my_cerver, "Welcome - Simple Test Message Example");
 
-	// my_cerver = cerver_create (CERVER_TYPE_CUSTOM, "my-cerver", get_port (argc, argv), PROTOCOL_TCP, false, 2, 2000);
-	// if (my_cerver) {
-	// 	cerver_set_welcome_msg (my_cerver, "Welcome - Simple Test Message Example");
+		/*** cerver configuration ***/
+		cerver_set_receive_buffer_size (my_cerver, 4096);
+		cerver_set_thpool_n_threads (my_cerver, 4);
 
-	// 	/*** cerver configuration ***/
-	// 	cerver_set_receive_buffer_size (my_cerver, 4096);
-	// 	cerver_set_thpool_n_threads (my_cerver, 4);
+		Handler *app_handler = handler_create (handler);
+		// 27/05/2020 - needed for this example!
+		handler_set_direct_handle (app_handler, true);
+		cerver_set_app_handlers (my_cerver, app_handler, NULL);
 
-	// 	Handler *app_handler = handler_create (handler);
-	// 	// 27/05/2020 - needed for this example!
-	// 	handler_set_direct_handle (app_handler, true);
-	// 	cerver_set_app_handlers (my_cerver, app_handler, NULL);
+		cerver_event_register (
+			my_cerver, 
+			CERVER_EVENT_CLIENT_CONNECTED,
+			on_client_connected, NULL, NULL,
+			false, false
+		);
 
-	// 	String *test = str_new ("This is a test!");
-	// 	cerver_event_register (
-	// 		my_cerver, 
-	// 		CERVER_EVENT_STARTED,
-	// 		on_cever_started, test, str_delete,
-	// 		false, false
-	// 	);
+		cerver_event_register (
+			my_cerver, 
+			CERVER_EVENT_CLIENT_CLOSE_CONNECTION,
+			on_client_close_connection, NULL, NULL,
+			false, false
+		);
 
-	// 	cerver_event_register (
-	// 		my_cerver, 
-	// 		CERVER_EVENT_TEARDOWN,
-	// 		on_cever_teardown, NULL, NULL,
-	// 		false, false
-	// 	);
+		if (cerver_start (my_cerver)) {
+			char *s = c_string_create ("Failed to start %s!",
+				my_cerver->info->name->str);
+			if (s) {
+				cerver_log_error (s);
+				free (s);
+			}
 
-	// 	cerver_event_register (
-	// 		my_cerver, 
-	// 		CERVER_EVENT_CLIENT_CONNECTED,
-	// 		on_client_connected, NULL, NULL,
-	// 		false, false
-	// 	);
+			cerver_delete (my_cerver);
+		}
+	}
 
-	// 	cerver_event_register (
-	// 		my_cerver, 
-	// 		CERVER_EVENT_CLIENT_CLOSE_CONNECTION,
-	// 		on_client_close_connection, NULL, NULL,
-	// 		false, false
-	// 	);
+	else {
+		cerver_log_error ("Failed to create cerver!");
 
-	// 	if (cerver_start (my_cerver)) {
-	// 		char *s = c_string_create ("Failed to start %s!",
-	// 			my_cerver->info->name->str);
-	// 		if (s) {
-	// 			cerver_log_error (s);
-	// 			free (s);
-	// 		}
+		cerver_delete (my_cerver);
+	}
 
-	// 		cerver_delete (my_cerver);
-	// 	}
-	// }
-
-	// else {
-	// 	cerver_log_error ("Failed to create cerver!");
-
-	// 	cerver_delete (my_cerver);
-	// }
+	cerver_end ();
 
 	return 0;
 
