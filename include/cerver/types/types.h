ceaa3c9b36bede695615acff8e462d4414d75e07
ermiry <erick_salas@ermiry.com>
2020-10-18 13:12:45 -0500

    Sync main types header

--- include/cerver/types/types.h
+++ include/cerver/types/types.h
@@ -13,8 +13,8 @@ typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
 
-typedef unsigned char ascii_char;
 typedef unsigned char uchar;
+typedef unsigned char ascii_char;
 
 // takes no argument and returns a value (int)
 typedef u8 (*Func)(void);
