e80b9dcafaf3a9a471525a23dfc750111d69fe2a
ermiry <erick_salas@ermiry.com>
2020-10-19 09:47:49 -0500

    Updated version to 1.7rc-9

--- include/cerver/version.h
+++ include/cerver/version.h
@@ -3,10 +3,10 @@
 
 #include "cerver/config.h"
 
-#define CERVER_VERSION                  "1.7rc-8"
-#define CERVER_VERSION_NAME             "Release 1.7rc-8"
-#define CERVER_VERSION_DATE			    "15/10/2020"
-#define CERVER_VERSION_TIME			    "06:14 CST"
+#define CERVER_VERSION                  "1.7rc-9"
+#define CERVER_VERSION_NAME             "Release 1.7rc-9"
+#define CERVER_VERSION_DATE			    "19/10/2020"
+#define CERVER_VERSION_TIME			    "09:47 CST"
 #define CERVER_VERSION_AUTHOR			"Erick Salas"
 
 // print full cerver version information 
