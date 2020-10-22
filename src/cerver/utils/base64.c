a8bc45da3afd278176c5d7e83b11cd42c0f62c6f
ermiry <erick_salas@ermiry.com>
2020-10-22 04:49:07 -0500

    Refactor utilities source structure

--- src/cerver/utils/base64.c
+++ src/cerver/utils/base64.c
@@ -1,70 +1,64 @@
 #include <string.h>
 
-/* ASCII table */
-/* aaaack but it's fast and const should make it shared text page. */
 static const unsigned char pr2six[256] = {
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
-    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
-    64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
-    64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
+	52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
+	64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
+	64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
 };
 
 int base64_decode (char *bufplain, const char *bufcoded) {
 
-    int nbytesdecoded = 0;
-    register const unsigned char *bufin = NULL;
-    register unsigned char *bufout = NULL;
-    register int nprbytes = 0;
+	int nbytesdecoded = 0;
+	register const unsigned char *bufin = NULL;
+	register unsigned char *bufout = NULL;
+	register int nprbytes = 0;
 
-    bufin = (const unsigned char *) bufcoded;
-    while (pr2six[*(bufin++)] <= 63);
-    nprbytes = (bufin - (const unsigned char *) bufcoded) - 1;
-    nbytesdecoded = ((nprbytes + 3) / 4) * 3;
+	bufin = (const unsigned char *) bufcoded;
+	while (pr2six[*(bufin++)] <= 63);
+	nprbytes = (bufin - (const unsigned char *) bufcoded) - 1;
+	nbytesdecoded = ((nprbytes + 3) / 4) * 3;
 
-    bufout = (unsigned char *) bufplain;
-    bufin = (const unsigned char *) bufcoded;
+	bufout = (unsigned char *) bufplain;
+	bufin = (const unsigned char *) bufcoded;
 
-    while (nprbytes > 4) {
-		*(bufout++) =
-			(unsigned char) (pr2six[*bufin] << 2 | pr2six[bufin[1]] >> 4);
-		*(bufout++) =
-			(unsigned char) (pr2six[bufin[1]] << 4 | pr2six[bufin[2]] >> 2);
-		*(bufout++) =
-			(unsigned char) (pr2six[bufin[2]] << 6 | pr2six[bufin[3]]);
+	while (nprbytes > 4) {
+		*(bufout++) = (unsigned char) (pr2six[*bufin] << 2 | pr2six[bufin[1]] >> 4);
+		*(bufout++) = (unsigned char) (pr2six[bufin[1]] << 4 | pr2six[bufin[2]] >> 2);
+		*(bufout++) = (unsigned char) (pr2six[bufin[2]] << 6 | pr2six[bufin[3]]);
 		bufin += 4;
 		nprbytes -= 4;
-    }
+	}
 
-    /* Note: (nprbytes == 1) would be an error, so just ingore that case */
-    if (nprbytes > 1) {
-    *(bufout++) =
-        (unsigned char) (pr2six[*bufin] << 2 | pr2six[bufin[1]] >> 4);
-    }
-    if (nprbytes > 2) {
-    *(bufout++) =
-        (unsigned char) (pr2six[bufin[1]] << 4 | pr2six[bufin[2]] >> 2);
-    }
-    if (nprbytes > 3) {
-    *(bufout++) =
-        (unsigned char) (pr2six[bufin[2]] << 6 | pr2six[bufin[3]]);
-    }
+	/* Note: (nprbytes == 1) would be an error, so just ingore that case */
+	if (nprbytes > 1) {
+		*(bufout++) = (unsigned char) (pr2six[*bufin] << 2 | pr2six[bufin[1]] >> 4);
+	}
 
-    *(bufout++) = '\0';
-    nbytesdecoded -= (4 - nprbytes) & 3;
+	if (nprbytes > 2) {
+		*(bufout++) = (unsigned char) (pr2six[bufin[1]] << 4 | pr2six[bufin[2]] >> 2);
+	}
 
-    return nbytesdecoded;
+	if (nprbytes > 3) {
+		*(bufout++) = (unsigned char) (pr2six[bufin[2]] << 6 | pr2six[bufin[3]]);
+	}
+
+	*(bufout++) = '\0';
+	nbytesdecoded -= (4 - nprbytes) & 3;
+
+	return nbytesdecoded;
 
 }
 
@@ -72,33 +66,32 @@ static const char basis_64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv
 
 int base64_encode (char *encoded, const char *string, int len) {
 
-    int i = 0;
-    char *p = encoded;
-    for (i = 0; i < len - 2; i += 3) {
+	int i = 0;
+	char *p = encoded;
+	for (i = 0; i < len - 2; i += 3) {
 		*p++ = basis_64[(string[i] >> 2) & 0x3F];
-		*p++ = basis_64[((string[i] & 0x3) << 4) |
-						((int) (string[i + 1] & 0xF0) >> 4)];
-		*p++ = basis_64[((string[i + 1] & 0xF) << 2) |
-						((int) (string[i + 2] & 0xC0) >> 6)];
+		*p++ = basis_64[((string[i] & 0x3) << 4) | ((int) (string[i + 1] & 0xF0) >> 4)];
+		*p++ = basis_64[((string[i + 1] & 0xF) << 2) | ((int) (string[i + 2] & 0xC0) >> 6)];
 		*p++ = basis_64[string[i + 2] & 0x3F];
-    }
+	}
 
-    if (i < len) {
+	if (i < len) {
 		*p++ = basis_64[(string[i] >> 2) & 0x3F];
 		if (i == (len - 1)) {
 			*p++ = basis_64[((string[i] & 0x3) << 4)];
 			*p++ = '=';
 		}
+
 		else {
-			*p++ = basis_64[((string[i] & 0x3) << 4) |
-							((int) (string[i + 1] & 0xF0) >> 4)];
+			*p++ = basis_64[((string[i] & 0x3) << 4) | ((int) (string[i + 1] & 0xF0) >> 4)];
 			*p++ = basis_64[((string[i + 1] & 0xF) << 2)];
 		}
+
 		*p++ = '=';
-    }
+	}
 
-    *p++ = '\0';
+	*p++ = '\0';
 
-    return p - encoded;
+	return p - encoded;
 
 }
--- src/cerver/utils/sha-256.c
+++ src/cerver/utils/sha-256.c
@@ -31,48 +31,52 @@ static const uint32_t k[] = {
 };
 
 struct buffer_state {
+
 	const uint8_t * p;
 	size_t len;
 	size_t total_len;
 	int single_one_delivered; /* bool */
 	int total_len_delivered; /* bool */
+
 };
 
-static inline uint32_t right_rot(uint32_t value, unsigned int count)
-{
-	/*
-	 * Defined behaviour in standard C for all count where 0 < count < 32,
-	 * which is what we need here.
-	 */
+/*
+* Defined behaviour in standard C for all count where 0 < count < 32,
+* which is what we need here.
+*/
+static inline uint32_t right_rot (uint32_t value, unsigned int count) {
+
 	return value >> count | value << (32 - count);
+
 }
 
-static void init_buf_state(struct buffer_state * state, const void * input, size_t len)
-{
+static void init_buf_state (struct buffer_state *state, const void *input, size_t len) {
+
 	state->p = (uint8_t *) input;
 	state->len = len;
 	state->total_len = len;
 	state->single_one_delivered = 0;
 	state->total_len_delivered = 0;
+
 }
 
 /* Return value: bool */
-static int calc_chunk(uint8_t chunk[CHUNK_SIZE], struct buffer_state * state)
-{
-	size_t space_in_chunk;
+static int calc_chunk (uint8_t chunk[CHUNK_SIZE], struct buffer_state * state) {
+
+	size_t space_in_chunk = 0;
 
 	if (state->total_len_delivered) {
 		return 0;
 	}
 
 	if (state->len >= CHUNK_SIZE) {
-		memcpy(chunk, state->p, CHUNK_SIZE);
+		memcpy (chunk, state->p, CHUNK_SIZE);
 		state->p += CHUNK_SIZE;
 		state->len -= CHUNK_SIZE;
 		return 1;
 	}
 
-	memcpy(chunk, state->p, state->len);
+	memcpy (chunk, state->p, state->len);
 	chunk += state->len;
 	space_in_chunk = CHUNK_SIZE - state->len;
 	state->p += state->len;
@@ -95,7 +99,7 @@ static int calc_chunk(uint8_t chunk[CHUNK_SIZE], struct buffer_state * state)
 		const size_t left = space_in_chunk - TOTAL_LEN_LEN;
 		size_t len = state->total_len;
 		int i;
-		memset(chunk, 0x00, left);
+		memset (chunk, 0x00, left);
 		chunk += left;
 
 		/* Storing of len * 8 as a big endian 64-bit without overflow. */
@@ -122,6 +126,7 @@ static int calc_chunk(uint8_t chunk[CHUNK_SIZE], struct buffer_state * state)
  *   In particular, the len parameter is a number of bytes.
  */
 void sha_256_calc (uint8_t hash[32], const void * input, size_t len) {
+
 	/*
 	 * Note 1: All integers (expect indexes) are 32-bit unsigned integers and addition is calculated modulo 2^32.
 	 * Note 2: For each round, there is one round constant k[i] and one entry in the message schedule array w[i], 0 = i = 63
@@ -136,18 +141,18 @@ void sha_256_calc (uint8_t hash[32], const void * input, size_t len) {
 	 * (first 32 bits of the fractional parts of the square roots of the first 8 primes 2..19):
 	 */
 	uint32_t h[] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
-	int i, j;
+	int i = 0, j = 0;
 
 	/* 512-bit chunks is what we will operate on. */
-	uint8_t chunk[64];
+	uint8_t chunk[64] = { 0 };
 
-	struct buffer_state state;
+	struct buffer_state state = { 0 };
 
-	init_buf_state(&state, input, len);
+	init_buf_state (&state, input, len);
 
-	while (calc_chunk(chunk, &state)) {
-		uint32_t ah[8];
-		
+	while (calc_chunk (chunk, &state)) {
+		uint32_t ah[8] = { 0 };
+
 		/*
 		 * create a 64-entry message schedule array w[0..63] of 32-bit words
 		 * (The initial values in w[0..63] don't matter, so many implementations zero them here)
@@ -156,7 +161,7 @@ void sha_256_calc (uint8_t hash[32], const void * input, size_t len) {
 		uint32_t w[64];
 		const uint8_t *p = chunk;
 
-		memset(w, 0x00, sizeof w);
+		memset (w, 0x00, sizeof w);
 		for (i = 0; i < 16; i++) {
 			w[i] = (uint32_t) p[0] << 24 | (uint32_t) p[1] << 16 |
 				(uint32_t) p[2] << 8 | (uint32_t) p[3];
@@ -165,21 +170,21 @@ void sha_256_calc (uint8_t hash[32], const void * input, size_t len) {
 
 		/* Extend the first 16 words into the remaining 48 words w[16..63] of the message schedule array: */
 		for (i = 16; i < 64; i++) {
-			const uint32_t s0 = right_rot(w[i - 15], 7) ^ right_rot(w[i - 15], 18) ^ (w[i - 15] >> 3);
-			const uint32_t s1 = right_rot(w[i - 2], 17) ^ right_rot(w[i - 2], 19) ^ (w[i - 2] >> 10);
+			const uint32_t s0 = right_rot (w[i - 15], 7) ^ right_rot (w[i - 15], 18) ^ (w[i - 15] >> 3);
+			const uint32_t s1 = right_rot (w[i - 2], 17) ^ right_rot (w[i - 2], 19) ^ (w[i - 2] >> 10);
 			w[i] = w[i - 16] + s0 + w[i - 7] + s1;
 		}
-		
+
 		/* Initialize working variables to current hash value: */
 		for (i = 0; i < 8; i++)
 			ah[i] = h[i];
 
 		/* Compression function main loop: */
 		for (i = 0; i < 64; i++) {
-			const uint32_t s1 = right_rot(ah[4], 6) ^ right_rot(ah[4], 11) ^ right_rot(ah[4], 25);
+			const uint32_t s1 = right_rot (ah[4], 6) ^ right_rot (ah[4], 11) ^ right_rot (ah[4], 25);
 			const uint32_t ch = (ah[4] & ah[5]) ^ (~ah[4] & ah[6]);
 			const uint32_t temp1 = ah[7] + s1 + ch + k[i] + w[i];
-			const uint32_t s0 = right_rot(ah[0], 2) ^ right_rot(ah[0], 13) ^ right_rot(ah[0], 22);
+			const uint32_t s0 = right_rot (ah[0], 2) ^ right_rot (ah[0], 13) ^ right_rot (ah[0], 22);
 			const uint32_t maj = (ah[0] & ah[1]) ^ (ah[0] & ah[2]) ^ (ah[1] & ah[2]);
 			const uint32_t temp2 = s0 + maj;
 
@@ -199,19 +204,19 @@ void sha_256_calc (uint8_t hash[32], const void * input, size_t len) {
 	}
 
 	/* Produce the final hash value (big-endian): */
-	for (i = 0, j = 0; i < 8; i++)
-	{
+	for (i = 0, j = 0; i < 8; i++) {
 		hash[j++] = (uint8_t) (h[i] >> 24);
 		hash[j++] = (uint8_t) (h[i] >> 16);
 		hash[j++] = (uint8_t) (h[i] >> 8);
 		hash[j++] = (uint8_t) h[i];
 	}
+
 }
 
 void sha_256_hash_to_string (char string[65], const uint8_t hash[32]) {
 
 	size_t i;
-	for (i = 0; i < 32; i++) 
+	for (i = 0; i < 32; i++)
 		string += sprintf(string, "%02x", hash[i]);
-	
-}	
+
+}
