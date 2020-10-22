b2a9839efb5c7807358bf93f38e33bdba9159397
ermiry <erick_salas@ermiry.com>
2020-10-22 04:48:33 -0500

    Changed game source indentation from spaces to tabs

--- src/cerver/game/gametype.c
+++ src/cerver/game/gametype.c
@@ -10,16 +10,16 @@ GameType *game_type_get_by_name (DoubleList *game_types, const char *name);
 
 GameType *game_type_new (void) {
 
-    GameType *type = (GameType *) malloc (sizeof (GameType));
-    if (type) {
-        memset (type, 0, sizeof (GameType));
-        type->name = NULL;
-        type->data = NULL;
-        type->delete_data = NULL;
-        type->start = type->end = NULL;
-    }
+	GameType *type = (GameType *) malloc (sizeof (GameType));
+	if (type) {
+		memset (type, 0, sizeof (GameType));
+		type->name = NULL;
+		type->data = NULL;
+		type->delete_data = NULL;
+		type->start = type->end = NULL;
+	}
 
-    return type;
+	return type;
 
 }
 
@@ -28,55 +28,55 @@ GameType *game_type_new (void) {
 // NULL on delete_data to use free by default
 // NULL on start or end is safe
 GameType *game_type_create (const char *name, void *data, void (*delete_data)(void *),
-    void *(*start)(void *), void *(*end) (void *)) {
+	void *(*start)(void *), void *(*end) (void *)) {
 
-    GameType *type = NULL;
-    if (name) {
-        type = game_type_new ();
-        if (type) {
-            type->name = str_new (name);
-            type->data = data;
-            type->delete_data = delete_data;
-            type->start = start;
-            type->end = end;
+	GameType *type = NULL;
+	if (name) {
+		type = game_type_new ();
+		if (type) {
+			type->name = str_new (name);
+			type->data = data;
+			type->delete_data = delete_data;
+			type->start = start;
+			type->end = end;
 
-            type->use_default_handler = true;
-            type->custom_handler = NULL;
-            type->max_players = 0;
-        }
-    }
+			type->use_default_handler = true;
+			type->custom_handler = NULL;
+			type->max_players = 0;
+		}
+	}
 
-    return type;
+	return type;
 
 }
 
 void game_type_delete (void *ptr) {
 
-    if (ptr) {
-        GameType *type = (GameType *) ptr;
-        str_delete (type->name);
-        if (type->delete_data) type->delete_data (type->data);
-        else free (type->data);
+	if (ptr) {
+		GameType *type = (GameType *) ptr;
+		str_delete (type->name);
+		if (type->delete_data) type->delete_data (type->data);
+		else free (type->data);
 
-        free (type);
-    }
+		free (type);
+	}
 
 }
 
 // add the required configuration that is needed to create a new lobby for the game type when requested
 // returns 0 on success, 1 on error
 int game_type_add_lobby_config (GameType *game_type,
-    bool use_default_handler, Action custom_handler, u32 max_players) {
+	bool use_default_handler, Action custom_handler, u32 max_players) {
 
-    int retval = 0;
+	int retval = 0;
 
-    if (game_type) {
-        game_type->use_default_handler = use_default_handler;
-        game_type->custom_handler = custom_handler;
-        game_type->max_players = max_players;
-    }
+	if (game_type) {
+		game_type->use_default_handler = use_default_handler;
+		game_type->custom_handler = custom_handler;
+		game_type->max_players = max_players;
+	}
 
-    return retval;
+	return retval;
 
 }
 
@@ -84,64 +84,64 @@ int game_type_add_lobby_config (GameType *game_type,
 // a reference to the player and lobby is passed as an argument
 void game_type_set_on_lobby_join (GameType *game_type, Action on_lobby_join) {
 
-    if (game_type) game_type->on_lobby_join = on_lobby_join;
+	if (game_type) game_type->on_lobby_join = on_lobby_join;
 
 }
 
 // sets an action to be called when a player leaves the lobby (game)
 // a reference to the player and lobby is passed as an argument
 void game_type_set_on_lobby_leave (GameType *game_type, Action on_lobby_leave) {
-    
-    if (game_type) game_type->on_lobby_leave = on_lobby_leave;
+
+	if (game_type) game_type->on_lobby_leave = on_lobby_leave;
 
 }
 
 // registers a new game type, returns 0 on LOG_TYPE_SUCCESS, 1 on error
 int game_type_register (DoubleList *game_types, GameType *game_type) {
 
-    int retval = 1;
+	int retval = 1;
 
-    if (game_type) {
-        retval = dlist_insert_after (game_types, 
-            dlist_end (game_types), game_type);
-    }
+	if (game_type) {
+		retval = dlist_insert_after (game_types,
+			dlist_end (game_types), game_type);
+	}
 
-    return retval;
+	return retval;
 
 }
 
 // unregister a game type, returns 0 on LOG_TYPE_SUCCESS, 1 on error
 int game_type_unregister (DoubleList *game_types, const char *name) {
 
-    int retval = 1;
+	int retval = 1;
 
-    if (name) {
-        GameType *game_type = NULL;
-        for (ListElement *le = dlist_start (game_types); le; le = le->next) {
-            game_type = (GameType *) le->data;
-            if (!strcmp (name, game_type->name->str)) {
-                game_type_delete (dlist_remove_element (game_types, le));
-                retval = 0;
-            }
-        }
-    }
+	if (name) {
+		GameType *game_type = NULL;
+		for (ListElement *le = dlist_start (game_types); le; le = le->next) {
+			game_type = (GameType *) le->data;
+			if (!strcmp (name, game_type->name->str)) {
+				game_type_delete (dlist_remove_element (game_types, le));
+				retval = 0;
+			}
+		}
+	}
 
-    return retval;
+	return retval;
 
 }
 
 // gets a registered game type by its name
 GameType *game_type_get_by_name (DoubleList *game_types, const char *name) {
 
-    GameType *game_type = NULL;
+	GameType *game_type = NULL;
 
-    if (name) {
-        for (ListElement *le = dlist_start (game_types); le; le = le->next) {
-            game_type = (GameType *) le->data;
-            if (!strcmp (name, game_type->name->str)) break;
-        }
-    }
+	if (name) {
+		for (ListElement *le = dlist_start (game_types); le; le = le->next) {
+			game_type = (GameType *) le->data;
+			if (!strcmp (name, game_type->name->str)) break;
+		}
+	}
 
-    return game_type;
+	return game_type;
 
 }
--- src/cerver/game/score.c
+++ src/cerver/game/score.c
@@ -26,9 +26,9 @@
 // scoreboard constructor
 ScoreBoard *game_score_new (void) {
 
-    ScoreBoard *sb = (ScoreBoard *) malloc (sizeof (ScoreBoard));
-    if (sb) memset (sb, 0, sizeof (ScoreBoard));
-    return sb;
+	ScoreBoard *sb = (ScoreBoard *) malloc (sizeof (ScoreBoard));
+	if (sb) memset (sb, 0, sizeof (ScoreBoard));
+	return sb;
 
 }
 
@@ -36,46 +36,46 @@ ScoreBoard *game_score_new (void) {
 // to be added each time we add a new player!!
 ScoreBoard *game_score_create (u8 playersNum, u8 scoresNum, ...) {
 
-    ScoreBoard *sb = (ScoreBoard *) malloc (sizeof (ScoreBoard));
-    if (sb) {
-        va_list valist;
-        va_start (valist, scoresNum);
+	ScoreBoard *sb = (ScoreBoard *) malloc (sizeof (ScoreBoard));
+	if (sb) {
+		va_list valist;
+		va_start (valist, scoresNum);
 
-        sb->scoresNum = scoresNum;
-        sb->scoreTypes = (char **) calloc (scoresNum, sizeof (char *));
-        char *temp = NULL;
-        for (int i = 0; i < scoresNum; i++) {
-            temp = va_arg (valist, char *);
-            sb->scoreTypes[i] = (char *) calloc (strlen (temp) + 1, sizeof (char));
-            strcpy (sb->scoreTypes[i], temp);
-        }
+		sb->scoresNum = scoresNum;
+		sb->scoreTypes = (char **) calloc (scoresNum, sizeof (char *));
+		char *temp = NULL;
+		for (int i = 0; i < scoresNum; i++) {
+			temp = va_arg (valist, char *);
+			sb->scoreTypes[i] = (char *) calloc (strlen (temp) + 1, sizeof (char));
+			strcpy (sb->scoreTypes[i], temp);
+		}
 
-        sb->registeredPlayers = 0;
+		sb->registeredPlayers = 0;
 
-        // FIXME: pass delete data
-        sb->scores = htab_create (
-            10,
-            NULL, NULL
-        );
+		// FIXME: pass delete data
+		sb->scores = htab_create (
+			10,
+			NULL, NULL
+		);
 
-        va_end (valist);
-    }
+		va_end (valist);
+	}
 
-    return sb;
+	return sb;
 
 }
 
 // destroy the scoreboard
 void game_score_delete (ScoreBoard *sb) {
 
-    if (sb) {
-        if (sb->scores) htab_destroy (sb->scores);
-        if (sb->scoreTypes) {
-            for (int i = 0; i < sb->scoresNum; i++) 
-                if (sb->scoreTypes[i]) free (sb->scoreTypes[i]);
-            
-        }
-    }
+	if (sb) {
+		if (sb->scores) htab_destroy (sb->scores);
+		if (sb->scoreTypes) {
+			for (int i = 0; i < sb->scoresNum; i++)
+				if (sb->scoreTypes[i]) free (sb->scoreTypes[i]);
+
+		}
+	}
 
 }
 
@@ -83,38 +83,38 @@ void game_score_delete (ScoreBoard *sb) {
 // add a new score type to all current players
 void game_score_add_scoreType (ScoreBoard *sb, char *newScore) {
 
-    if (sb && newScore) {
-        // first check if we have that score type
-        bool found = false;
-        for (int i = 0; i < sb->scoresNum; i++) {
-            if (!strcmp (sb->scoreTypes[i], newScore)) {
-                found = true;
-                break;
-            }
-        }
+	if (sb && newScore) {
+		// first check if we have that score type
+		bool found = false;
+		for (int i = 0; i < sb->scoresNum; i++) {
+			if (!strcmp (sb->scoreTypes[i], newScore)) {
+				found = true;
+				break;
+			}
+		}
 
-        if (found) {
-            // add the new score to all the players
-            // FIXME:
+		if (found) {
+			// add the new score to all the players
+			// FIXME:
 
-            // add the score to the scoretypes
-            sb->scoreTypes = (char **) realloc (sb->scores, sb->scoresNum);
-            if (sb->scoreTypes) {
-                sb->scoreTypes[sb->scoresNum] = (char *) calloc (strlen (newScore) + 1, sizeof (char));
-                strcpy (sb->scoreTypes[sb->scoresNum], newScore);
-                sb->scoresNum++;
-            } 
-        }
+			// add the score to the scoretypes
+			sb->scoreTypes = (char **) realloc (sb->scores, sb->scoresNum);
+			if (sb->scoreTypes) {
+				sb->scoreTypes[sb->scoresNum] = (char *) calloc (strlen (newScore) + 1, sizeof (char));
+				strcpy (sb->scoreTypes[sb->scoresNum], newScore);
+				sb->scoresNum++;
+			}
+		}
 
-        else {
-            #ifdef CERVER_DEBUG
-            cerver_log (
-                LOG_TYPE_ERROR, LOG_TYPE_GAME, 
-                "Can't add score type! It already exists in the scoreboard."
-            );
-            #endif
-        }
-    }
+		else {
+			#ifdef CERVER_DEBUG
+			cerver_log (
+				LOG_TYPE_ERROR, LOG_TYPE_GAME,
+				"Can't add score type! It already exists in the scoreboard."
+			);
+			#endif
+		}
+	}
 
 }
 
@@ -122,262 +122,262 @@ void game_score_add_scoreType (ScoreBoard *sb, char *newScore) {
 // remove a score type from all current players
 u8 game_score_remove_scoreType (ScoreBoard *sb, char *oldScore) {
 
-    u8 retval = 1;
+	u8 retval = 1;
 
-    if (sb && oldScore) {
-        // first check if we have that score type
-        int found = -1;
-        for (int i = 0; i < sb->scoresNum; i++) {
-            if (!strcmp (sb->scoreTypes[i], oldScore)) {
-                found = i;
-                break;
-            }
-        }
+	if (sb && oldScore) {
+		// first check if we have that score type
+		int found = -1;
+		for (int i = 0; i < sb->scoresNum; i++) {
+			if (!strcmp (sb->scoreTypes[i], oldScore)) {
+				found = i;
+				break;
+			}
+		}
 
-        if (found > 0) {
-            // remove the score from all the players
-            // FIXME:
+		if (found > 0) {
+			// remove the score from all the players
+			// FIXME:
 
-            // manually copy old scores array and delete it
-            // this is done because we allocate exat space for each score type!
-            char **new_scoreTypes = (char **) calloc (sb->scoresNum - 1, sizeof (char *));
-            int i = 0;
-            while (i < found) {
-                new_scoreTypes[i] = (char *) calloc (strlen (sb->scoreTypes[i]) + 1, sizeof (char));
-                strcpy (new_scoreTypes[i], sb->scoreTypes[i]);
-                i++;
-            }
+			// manually copy old scores array and delete it
+			// this is done because we allocate exat space for each score type!
+			char **new_scoreTypes = (char **) calloc (sb->scoresNum - 1, sizeof (char *));
+			int i = 0;
+			while (i < found) {
+				new_scoreTypes[i] = (char *) calloc (strlen (sb->scoreTypes[i]) + 1, sizeof (char));
+				strcpy (new_scoreTypes[i], sb->scoreTypes[i]);
+				i++;
+			}
 
-            for (i = found + 1; i < sb->scoresNum; i++) {
-                new_scoreTypes[i - 1] = (char *) calloc (strlen (sb->scoreTypes[i]) + 1, sizeof (char));
-                strcpy (new_scoreTypes[i - 1], sb->scoreTypes[i]);
-            }
+			for (i = found + 1; i < sb->scoresNum; i++) {
+				new_scoreTypes[i - 1] = (char *) calloc (strlen (sb->scoreTypes[i]) + 1, sizeof (char));
+				strcpy (new_scoreTypes[i - 1], sb->scoreTypes[i]);
+			}
 
-            sb->scoresNum--;
+			sb->scoresNum--;
 
-            retval = 0;
-        }
+			retval = 0;
+		}
 
-        else {
-            #ifdef CERVER_DEBUG
-            cerver_log (
-                LOG_TYPE_ERROR, LOG_TYPE_GAME,
-                "Can't remove %s scoretype, doesn't exist in the scoreboard!"
-            );
-            #endif
-        }
-    }
+		else {
+			#ifdef CERVER_DEBUG
+			cerver_log (
+				LOG_TYPE_ERROR, LOG_TYPE_GAME,
+				"Can't remove %s scoretype, doesn't exist in the scoreboard!"
+			);
+			#endif
+		}
+	}
 
-    return retval;
+	return retval;
 
 }
 
 // adds a new player to the score table
 u8 game_score_add_player (ScoreBoard *sb, char *playerName) {
 
-    if (sb && playerName) {
-        if (htab_contains_key (sb->scores, playerName, sizeof (playerName))) {
-            #ifdef CERVER_DEBUG
-            cerver_log (
-                LOG_TYPE_ERROR, LOG_TYPE_GAME,
-                "Scores table already contains player: %s", playerName
-            );
-            #endif
-            return 1;
-        }
+	if (sb && playerName) {
+		if (htab_contains_key (sb->scores, playerName, sizeof (playerName))) {
+			#ifdef CERVER_DEBUG
+			cerver_log (
+				LOG_TYPE_ERROR, LOG_TYPE_GAME,
+				"Scores table already contains player: %s", playerName
+			);
+			#endif
+			return 1;
+		}
 
-        // associate the player with his own scores dictionary
-        else {
-            // FIXME: correctly destroy this
-            Htab *newHt = htab_create (sb->scoresNum, NULL, NULL);
-            if (newHt) {
-                if (!htab_insert (sb->scores, playerName, sizeof (playerName), newHt, sizeof (Htab))) {
-                    // insert each score type in the player's dictionary
-                    u32 zero = 0;
-                    for (int i = 0; i < sb->scoresNum; i++) 
-                        htab_insert (newHt, sb->scoreTypes[i], 
-                            sizeof (sb->scoreTypes[i]), &zero, sizeof (int));
+		// associate the player with his own scores dictionary
+		else {
+			// FIXME: correctly destroy this
+			Htab *newHt = htab_create (sb->scoresNum, NULL, NULL);
+			if (newHt) {
+				if (!htab_insert (sb->scores, playerName, sizeof (playerName), newHt, sizeof (Htab))) {
+					// insert each score type in the player's dictionary
+					u32 zero = 0;
+					for (int i = 0; i < sb->scoresNum; i++)
+						htab_insert (newHt, sb->scoreTypes[i],
+							sizeof (sb->scoreTypes[i]), &zero, sizeof (int));
 
-                    sb->registeredPlayers++;
+					sb->registeredPlayers++;
 
-                    return 0;   // LOG_TYPE_SUCCESS adding new player and its scores
-                }
-            }
-        }
-    }
+					return 0;   // LOG_TYPE_SUCCESS adding new player and its scores
+				}
+			}
+		}
+	}
 
-    return 1;
+	return 1;
 
 }
 
 // FIXME: how do we get the player name?? the server is not interested in knowing a playe's name
-// inside his player structure, it is the admin reponsability to determine how to get 
+// inside his player structure, it is the admin reponsability to determine how to get
 // the player's name
 void game_score_add_lobby_players (ScoreBoard *sb, AVLNode *playerNode) {
 
-    if (sb && playerNode) {
-        game_score_add_lobby_players (sb, playerNode->right);
+	if (sb && playerNode) {
+		game_score_add_lobby_players (sb, playerNode->right);
 
-        // if (playerNode->id) game_score_add_player (sb, ((Player *) playerNode->id));
+		// if (playerNode->id) game_score_add_player (sb, ((Player *) playerNode->id));
 
-        game_score_add_lobby_players (sb, playerNode->left);
-    }
+		game_score_add_lobby_players (sb, playerNode->left);
+	}
 
 }
 
 // removes a player from the score table
 u8 game_score_remove_player (ScoreBoard *sb, char *playerName) {
 
-    if (sb && playerName) {
-        if (htab_contains_key (sb->scores, playerName, sizeof (playerName))) {
-            // size_t htab_size = sizeof (Htab);
-            void *htab = htab_get (sb->scores, playerName, sizeof (playerName));
-            
-            // destroy player's scores htab
-            if (htab) htab_destroy ((Htab *) htab);
+	if (sb && playerName) {
+		if (htab_contains_key (sb->scores, playerName, sizeof (playerName))) {
+			// size_t htab_size = sizeof (Htab);
+			void *htab = htab_get (sb->scores, playerName, sizeof (playerName));
 
-            // remove th player from the scoreboard
-            htab_remove (sb->scores, playerName, sizeof (playerName));
+			// destroy player's scores htab
+			if (htab) htab_destroy ((Htab *) htab);
 
-            sb->registeredPlayers--;
-        }
+			// remove th player from the scoreboard
+			htab_remove (sb->scores, playerName, sizeof (playerName));
 
-        else {
-            #ifdef CERVER_DEBUG
-            cerver_log (
-                LOG_TYPE_ERROR, LOG_TYPE_GAME,
-                "Scores table doesn't contains player: %s", playerName
-            );
-            #endif
-        }
-    }
+			sb->registeredPlayers--;
+		}
 
-    return 1;   // error
+		else {
+			#ifdef CERVER_DEBUG
+			cerver_log (
+				LOG_TYPE_ERROR, LOG_TYPE_GAME,
+				"Scores table doesn't contains player: %s", playerName
+			);
+			#endif
+		}
+	}
+
+	return 1;   // error
 
 }
 
 // sets the score's value, negative values not allowed
 void game_score_set (ScoreBoard *sb, char *playerName, char *scoreType, i32 value) {
 
-    if (sb && playerName && scoreType) {
-        // size_t htab_size = sizeof (Htab);
-        void *playerScores = htab_get (sb->scores, playerName, sizeof (playerName));
-        if (playerScores) {
-            // size_t int_size = sizeof (unsigned int);
-            void *currValue = htab_get ((Htab *) playerScores, 
-                scoreType, sizeof (scoreType));
+	if (sb && playerName && scoreType) {
+		// size_t htab_size = sizeof (Htab);
+		void *playerScores = htab_get (sb->scores, playerName, sizeof (playerName));
+		if (playerScores) {
+			// size_t int_size = sizeof (unsigned int);
+			void *currValue = htab_get ((Htab *) playerScores,
+				scoreType, sizeof (scoreType));
 
-            // replace the old value with the new one
-            if (currValue) {
-                u32 *current = (u32 *) currValue;
-                if (value > 0) *current = value;
-                else *current = 0;
-            }
-        }
+			// replace the old value with the new one
+			if (currValue) {
+				u32 *current = (u32 *) currValue;
+				if (value > 0) *current = value;
+				else *current = 0;
+			}
+		}
 
-        else {
-            #ifdef CERVER_DEBUG
-            cerver_log (
-                LOG_TYPE_ERROR, LOG_TYPE_GAME,
-                "Player: %s has not scores in the table!", playerName
-            );
-            #endif
-        }
-    }
+		else {
+			#ifdef CERVER_DEBUG
+			cerver_log (
+				LOG_TYPE_ERROR, LOG_TYPE_GAME,
+				"Player: %s has not scores in the table!", playerName
+			);
+			#endif
+		}
+	}
 
 }
 
 // gets the value of the player's score type
 i32 game_score_get (ScoreBoard *sb, char *playerName, char *scoreType) {
 
-    if (sb && playerName && scoreType) {
-        // size_t htab_size = sizeof (Htab);
-        void *playerScores = htab_get (sb->scores, playerName, sizeof (playerName));
-        if (playerScores) {
-            // size_t int_size = sizeof (unsigned int);
-            void *value = htab_get ((Htab *) playerScores, 
-                scoreType, sizeof (scoreType));
+	if (sb && playerName && scoreType) {
+		// size_t htab_size = sizeof (Htab);
+		void *playerScores = htab_get (sb->scores, playerName, sizeof (playerName));
+		if (playerScores) {
+			// size_t int_size = sizeof (unsigned int);
+			void *value = htab_get ((Htab *) playerScores,
+				scoreType, sizeof (scoreType));
 
-            if (value) {
-                u32 *retval = (u32 *) value;
-                return *retval;
-            }
-        }
+			if (value) {
+				u32 *retval = (u32 *) value;
+				return *retval;
+			}
+		}
 
-        else {
-            #ifdef CERVER_DEBUG
-            cerver_log (
-                LOG_TYPE_ERROR, LOG_TYPE_GAME,
-                "Player: %s has not scores in the table!", playerName
-            );
-            #endif
-        }
-    }
+		else {
+			#ifdef CERVER_DEBUG
+			cerver_log (
+				LOG_TYPE_ERROR, LOG_TYPE_GAME,
+				"Player: %s has not scores in the table!", playerName
+			);
+			#endif
+		}
+	}
 
-    return -1;  // error
+	return -1;  // error
 
 }
 
 // updates the value of the player's score type, clamped to 0
 void game_score_update (ScoreBoard *sb, char *playerName, char *scoreType, i32 value) {
 
-    if (sb && playerName && scoreType) {
-        // size_t htab_size = sizeof (Htab);
-        void *playerScores = htab_get (sb->scores, playerName, sizeof (playerName));
-        if (playerScores) {
-            // size_t int_size = sizeof (unsigned int);
-            void *currValue = htab_get ((Htab *) playerScores, 
-                scoreType, sizeof (scoreType));
-            
-            // directly update the score value adding the new value
-            if (value) {
-                u32 *current = (u32 *) currValue;
-                if ((*current += value) < 0) *current = 0;
-                else *current += value;
-            }
-        }
+	if (sb && playerName && scoreType) {
+		// size_t htab_size = sizeof (Htab);
+		void *playerScores = htab_get (sb->scores, playerName, sizeof (playerName));
+		if (playerScores) {
+			// size_t int_size = sizeof (unsigned int);
+			void *currValue = htab_get ((Htab *) playerScores,
+				scoreType, sizeof (scoreType));
 
-        else {
-            #ifdef CERVER_DEBUG
-            cerver_log (
-                LOG_TYPE_ERROR, LOG_TYPE_GAME,
-                "Player: %s has not scores in the table!", playerName
-            );
-            #endif
-        }
-    }
+			// directly update the score value adding the new value
+			if (value) {
+				u32 *current = (u32 *) currValue;
+				if ((*current += value) < 0) *current = 0;
+				else *current += value;
+			}
+		}
+
+		else {
+			#ifdef CERVER_DEBUG
+			cerver_log (
+				LOG_TYPE_ERROR, LOG_TYPE_GAME,
+				"Player: %s has not scores in the table!", playerName
+			);
+			#endif
+		}
+	}
 
 }
 
 // reset all the player's scores
 void game_score_reset (ScoreBoard *sb, char *playerName) {
 
-    if (sb && playerName) {
-        // size_t htab_size = sizeof (Htab);
-        void *data = htab_get (sb->scores, playerName, sizeof (playerName));
-        if (data) {
-            Htab *playerScores = (Htab *) data;
-            void *scoreData = NULL;
-            // size_t score_size = sizeof (u32);
-            for (int i = 0; i < sb->scoresNum; i++) {
-                scoreData = htab_get (playerScores, 
-                    sb->scoreTypes[i], sizeof (sb->scoreTypes[i]));
+	if (sb && playerName) {
+		// size_t htab_size = sizeof (Htab);
+		void *data = htab_get (sb->scores, playerName, sizeof (playerName));
+		if (data) {
+			Htab *playerScores = (Htab *) data;
+			void *scoreData = NULL;
+			// size_t score_size = sizeof (u32);
+			for (int i = 0; i < sb->scoresNum; i++) {
+				scoreData = htab_get (playerScores,
+					sb->scoreTypes[i], sizeof (sb->scoreTypes[i]));
 
-                if (scoreData) {
-                    u32 *current = (u32 *) scoreData;
-                    *current = 0;
-                }
-            }
-        }
+				if (scoreData) {
+					u32 *current = (u32 *) scoreData;
+					*current = 0;
+				}
+			}
+		}
 
-        else {
-            #ifdef CERVER_DEBUG
-            cerver_log (
-                LOG_TYPE_ERROR, LOG_TYPE_GAME,
-                "Scores table already contains player: %s", playerName
-            );
-            #endif
-        }
-    }
+		else {
+			#ifdef CERVER_DEBUG
+			cerver_log (
+				LOG_TYPE_ERROR, LOG_TYPE_GAME,
+				"Scores table already contains player: %s", playerName
+			);
+			#endif
+		}
+	}
 
 }
