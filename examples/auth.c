b3ca5e0695a14fdf08230403f225e22e09a5937a
ermiry <erick_salas@ermiry.com>
2020-11-06 12:10:29 -0600

    Testing auth example with threads handler

--- examples/auth.c
+++ examples/auth.c
@@ -326,6 +326,9 @@ int main (void) {
 		cerver_set_receive_buffer_size (my_cerver, 4096);
 		// cerver_set_thpool_n_threads (my_cerver, 4);
 
+		cerver_set_handler_type (my_cerver, CERVER_HANDLER_TYPE_THREADS);
+		cerver_set_handle_detachable_threads (my_cerver, true);
+
 		Handler *app_handler = handler_create (handler);
 		handler_set_direct_handle (app_handler, true);
 		cerver_set_app_handlers (my_cerver, app_handler, NULL);
