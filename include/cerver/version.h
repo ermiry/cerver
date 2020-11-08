9897c043a2e2be06eb67278da7a75e17adff1e34
ermiry <erick_salas@ermiry.com>
2020-11-08 16:36:44 -0600

    Updated version to Beta 2.0b-12

--- include/cerver/version.h
+++ include/cerver/version.h
@@ -3,10 +3,10 @@
 
 #include "cerver/config.h"
 
-#define CERVER_VERSION                  "2.0b-11"
-#define CERVER_VERSION_NAME             "Beta 2.0b-11"
-#define CERVER_VERSION_DATE			    "07/11/2020"
-#define CERVER_VERSION_TIME			    "17:25 CST"
+#define CERVER_VERSION                  "2.0b-12"
+#define CERVER_VERSION_NAME             "Beta 2.0b-12"
+#define CERVER_VERSION_DATE			    "08/11/2020"
+#define CERVER_VERSION_TIME			    "16:35 CST"
 #define CERVER_VERSION_AUTHOR			"Erick Salas"
 
 // print full cerver version information
