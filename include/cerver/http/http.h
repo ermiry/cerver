ed6de30d0dff8a379f9b943cc5967bea7d77d0a8
ermiry <erick_salas@ermiry.com>
2020-11-25 05:34:01 -0600

    Refactored main http header methods declarations organization

--- include/cerver/http/http.h
+++ include/cerver/http/http.h
@@ -126,30 +126,36 @@ typedef struct HttpStaticPath {
 } HttpStaticPath;
 
 // sets authentication requirenments for a whole static path
-CERVER_EXPORT void http_static_path_set_auth (HttpStaticPath *static_path, HttpRouteAuthType auth_type);
+CERVER_EXPORT void http_static_path_set_auth (
+	HttpStaticPath *static_path, HttpRouteAuthType auth_type
+);
 
 // add a new static path where static files can be served upon request
 // it is recomened to set absoulute paths
-CERVER_EXPORT HttpStaticPath *http_cerver_static_path_add (HttpCerver *http_cerver, const char *static_path);
+CERVER_EXPORT HttpStaticPath *http_cerver_static_path_add (
+	HttpCerver *http_cerver, const char *static_path
+);
 
 // removes a path from the served public paths
 // returns 0 on success, 1 on error
-CERVER_EXPORT u8 http_receive_public_path_remove (HttpCerver *http_cerver, const char *static_path);
+CERVER_EXPORT u8 http_receive_public_path_remove (
+	HttpCerver *http_cerver, const char *static_path
+);
 
 #pragma endregion
 
 #pragma region routes
 
 // register a new http to the http cerver
-CERVER_EXPORT void http_cerver_route_register (HttpCerver *http_cerver, HttpRoute *route);
+CERVER_EXPORT void http_cerver_route_register (
+	HttpCerver *http_cerver, HttpRoute *route
+);
 
 // set a route to catch any requet that didn't match any registered route
-CERVER_EXPORT void http_cerver_set_catch_all_route (HttpCerver *http_cerver, 
-	void (*catch_all_route)(CerverReceive *cr, HttpRequest *request));
-
-// generates and signs a jwt token that is ready to be sent
-// returns a newly allocated string that should be deleted after use
-CERVER_EXPORT char *http_cerver_auth_generate_jwt (HttpCerver *http_cerver, DoubleList *values);
+CERVER_EXPORT void http_cerver_set_catch_all_route (
+	HttpCerver *http_cerver, 
+	void (*catch_all_route)(CerverReceive *cr, HttpRequest *request)
+);
 
 #pragma endregion
 
@@ -157,11 +163,15 @@ CERVER_EXPORT char *http_cerver_auth_generate_jwt (HttpCerver *http_cerver, Doub
 
 // sets the default uploads path where any multipart file request will be saved
 // this method will replace the previous value with the new one
-CERVER_EXPORT void http_cerver_set_uploads_path (HttpCerver *http_cerver, const char *uploads_path);
+CERVER_EXPORT void http_cerver_set_uploads_path (
+	HttpCerver *http_cerver, const char *uploads_path
+);
 
 // sets a method to be called on every new request & that will be used to generate a new directory
 // inside the uploads path to save all the files from each request
-CERVER_EXPORT void http_cerver_set_uploads_dirname_generator (HttpCerver *http_cerver, String *(*dirname_generator)(CerverReceive *));
+CERVER_EXPORT void http_cerver_set_uploads_dirname_generator (
+	HttpCerver *http_cerver, String *(*dirname_generator)(CerverReceive *)
+);
 
 #pragma endregion
 
@@ -169,13 +179,25 @@ CERVER_EXPORT void http_cerver_set_uploads_dirname_generator (HttpCerver *http_c
 
 // sets the jwt algorithm used for encoding & decoding jwt tokens
 // the default value is JWT_ALG_HS256
-CERVER_EXPORT void http_cerver_auth_set_jwt_algorithm (HttpCerver *http_cerver, jwt_alg_t jwt_alg);
+CERVER_EXPORT void http_cerver_auth_set_jwt_algorithm (
+	HttpCerver *http_cerver, jwt_alg_t jwt_alg
+);
 
 // sets the filename from where the jwt private key will be loaded
-CERVER_EXPORT void http_cerver_auth_set_jwt_priv_key_filename (HttpCerver *http_cerver, const char *filename);
+CERVER_EXPORT void http_cerver_auth_set_jwt_priv_key_filename (
+	HttpCerver *http_cerver, const char *filename
+);
 
 // sets the filename from where the jwt public key will be loaded
-CERVER_EXPORT void http_cerver_auth_set_jwt_pub_key_filename (HttpCerver *http_cerver, const char *filename);
+CERVER_EXPORT void http_cerver_auth_set_jwt_pub_key_filename (
+	HttpCerver *http_cerver, const char *filename
+);
+
+// generates and signs a jwt token that is ready to be sent
+// returns a newly allocated string that should be deleted after use
+CERVER_EXPORT char *http_cerver_auth_generate_jwt (
+	HttpCerver *http_cerver, DoubleList *values
+);
 
 #pragma endregion
 
@@ -205,10 +227,14 @@ CERVER_PUBLIC char *http_url_decode (const char *str);
 #pragma region parser
 
 // parses a url query into key value pairs for better manipulation
-CERVER_PUBLIC DoubleList *http_parse_query_into_pairs (const char *first, const char *last);
+CERVER_PUBLIC DoubleList *http_parse_query_into_pairs (
+	const char *first, const char *last
+);
 
 // gets the matching value for the requested key from a list of pairs
-CERVER_PUBLIC const String *http_query_pairs_get_value (DoubleList *pairs, const char *key);
+CERVER_PUBLIC const String *http_query_pairs_get_value (
+	DoubleList *pairs, const char *key
+);
 
 CERVER_PUBLIC void http_query_pairs_print (DoubleList *pairs);
 
