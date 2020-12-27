538e3d17be8704b7339f4ca05af90bee7fbbb0a7
ermiry <erick_salas@ermiry.com>
2020-12-27 02:49:34 -0600

    Refactored test where macro & added more definitions

--- test/test.h
+++ test/test.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define where (void) fprintf (stderr, "%s - %d: ", __FILE__, __LINE__)
+#define where (void) fprintf (stderr, "%s:%d -> ", __FILE__, __LINE__)
 
 #define											\
 	fail(msg) 									\
@@ -18,30 +18,52 @@
 	test_check(result, msg)											\
 	({																\
 		if ((result) == false) {									\
+			where;													\
 			(void) fprintf (										\
 				stderr,												\
 				"Check has failed!\n"								\
 			);														\
 																	\
-			where;													\
 			if (msg) (void) fprintf (stderr, "%s\n", (char *) msg);	\
 			exit (1);												\
 		}															\
 	})
 
+#define test_check_ptr_eq(X, Y) test_check (X == Y, NULL)
+
 #define test_check_ptr_ne(X, Y) test_check (X != Y, NULL)
+
+#define test_check_int_ne(X, Y) test_check (X != Y, NULL)
+
+#define test_check_int_gt(X, Y) test_check (X > Y, NULL)
 
 #define																\
 	test_check_int_eq(value, expected, msg)							\
 	({																\
 		if (value != expected) {									\
+			where;													\
 			(void) fprintf (										\
 				stderr,												\
 				"INTEGER %d does not match %d\n",					\
 				value, expected										\
 			);														\
 																	\
+			if (msg) (void) fprintf (stderr, "%s\n", (char *) msg);	\
+			exit (1);												\
+		}															\
+	})
+
+#define																\
+	test_check_long_int_eq(value, expected, msg)					\
+	({																\
+		if (value != expected) {									\
 			where;													\
+			(void) fprintf (										\
+				stderr,												\
+				"INTEGER %ld does not match %ld\n",					\
+				value, expected										\
+			);														\
+																	\
 			if (msg) (void) fprintf (stderr, "%s\n", (char *) msg);	\
 			exit (1);												\
 		}															\
@@ -51,13 +73,13 @@
 	test_check_str_eq(value, expected, msg)							\
 	({																\
 		if (strcmp ((char *) value, (char *) expected)) {			\
+			where;													\
 			(void) fprintf (										\
 				stderr,												\
 				"STRING %s does not match %s\n",					\
 				(char *) value, (char *) expected					\
 			);														\
 																	\
-			where;													\
 			if (msg) (void) fprintf (stderr, "%s\n", (char *) msg);	\
 			exit (1);												\
 		}															\
