7bec34ce3acf7f96757b55d9596551a032d8e2cb
ermiry <erick_salas@ermiry.com>
2020-12-26 23:45:45 -0600

    Refactored version methods to use cerver log

--- src/cerver/version.c
+++ src/cerver/version.c
@@ -5,22 +5,43 @@
 // print full erver version information
 void cerver_version_print_full (void) {
 
-	cerver_log_msg ("\nCerver Version: %s", CERVER_VERSION_NAME);
-	cerver_log_msg ("Release Date & time: %s - %s", CERVER_VERSION_DATE, CERVER_VERSION_TIME);
-	cerver_log_msg ("Author: %s\n", CERVER_VERSION_AUTHOR);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"\nCerver Version: %s", CERVER_VERSION_NAME
+	);
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Release Date & time: %s - %s",
+		CERVER_VERSION_DATE, CERVER_VERSION_TIME
+	);
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Author: %s\n",
+		CERVER_VERSION_AUTHOR
+	);
 
 }
 
 // print the version id
 void cerver_version_print_version_id (void) {
 
-	cerver_log_msg ("\nCerver Version ID: %s\n", CERVER_VERSION);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"\nCerver Version ID: %s\n",
+		CERVER_VERSION
+	);
 
 }
 
 // print the version name
 void cerver_version_print_version_name (void) {
 
-	cerver_log_msg ("\nCerver Version: %s\n", CERVER_VERSION_NAME);
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"\nCerver Version: %s\n",
+		CERVER_VERSION_NAME
+	);
 
 }
