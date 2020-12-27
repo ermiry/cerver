ba74a33fd89cdb512502f83160b1286c234fc894
ermiry <erick_salas@ermiry.com>
2020-12-27 02:50:26 -0600

    Refactored jwt_str_alg () to use strcasecmp () to increase compatibility

--- src/cerver/http/jwt/alg.c
+++ src/cerver/http/jwt/alg.c
@@ -17,16 +17,16 @@ const char *jwt_alg_str (jwt_alg_t alg) {
 
 jwt_alg_t jwt_str_alg (const char *alg) {
 
-	if (!strcmp (alg, "none")) return JWT_ALG_NONE;
-	else if (!strcmp (alg, "HS256")) return JWT_ALG_HS256;
-	else if (!strcmp (alg, "HS384")) return JWT_ALG_HS384;
-	else if (!strcmp (alg, "HS512")) return JWT_ALG_HS512;
-	else if (!strcmp (alg, "RS256")) return JWT_ALG_RS256;
-	else if (!strcmp (alg, "RS384")) return JWT_ALG_RS384;
-	else if (!strcmp (alg, "RS512")) return JWT_ALG_RS512;
-	else if (!strcmp (alg, "ES256")) return JWT_ALG_ES256;
-	else if (!strcmp (alg, "ES384")) return JWT_ALG_ES384;
-	else if (!strcmp (alg, "ES512")) return JWT_ALG_ES512;
+	if (!strcasecmp (alg, "none")) return JWT_ALG_NONE;
+	else if (!strcasecmp (alg, "HS256")) return JWT_ALG_HS256;
+	else if (!strcasecmp (alg, "HS384")) return JWT_ALG_HS384;
+	else if (!strcasecmp (alg, "HS512")) return JWT_ALG_HS512;
+	else if (!strcasecmp (alg, "RS256")) return JWT_ALG_RS256;
+	else if (!strcasecmp (alg, "RS384")) return JWT_ALG_RS384;
+	else if (!strcasecmp (alg, "RS512")) return JWT_ALG_RS512;
+	else if (!strcasecmp (alg, "ES256")) return JWT_ALG_ES256;
+	else if (!strcasecmp (alg, "ES384")) return JWT_ALG_ES384;
+	else if (!strcasecmp (alg, "ES512")) return JWT_ALG_ES512;
 
 	return JWT_ALG_INVAL;
 
