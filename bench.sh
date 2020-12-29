89c4df8d2273b7ce97168e851710fdf34722e9f7
ermiry <erick.salas@ermiry.com>
2020-12-28 21:34:50 -0600

    Refactored bench script to remove examples debug & added steps labels

--- bench.sh
+++ bench.sh
@@ -3,7 +3,14 @@
 # ensure a clean build
 make clean
 
+printf "sources\n\n"
 make DEVELOPMENT='' -j8
-make DEVELOPMENT='' examples -j8
+
+printf "\n\nexamples\n\n"
+make DEVELOPMENT='' EXADEBUG='' examples -j8
+
+printf "\n\ntest\n\n"
 make DEVELOPMENT='' test -j8
+
+printf "\n\nbench\n\n"
 make DEVELOPMENT='' bench -j8
