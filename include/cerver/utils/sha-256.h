e8286243182e8bc6cd40da4ac80e7b9012507e32
ermiry <erick.salas@ermiry.com>
2020-12-31 12:43:49 -0600

    Updated sha256 implementation and added user friendly methods

--- include/cerver/utils/sha-256.h
+++ include/cerver/utils/sha-256.h
@@ -5,8 +5,26 @@
 
 #include "cerver/config.h"
 
-CERVER_PUBLIC void sha_256_calc (uint8_t hash[32], const void *input, size_t len);
+#define SHA256_STRING_LEN		80
 
-CERVER_PUBLIC void sha_256_hash_to_string (char string[65], const uint8_t hash[32]);
+CERVER_PUBLIC void sha_256_calc (
+	uint8_t hash[32], const void *input, size_t len
+);
+
+CERVER_PUBLIC void sha_256_hash_to_string (
+	char string[65], const uint8_t hash[32]
+);
+
+// generates a sha256 string from the input and places it in output
+// output must be at least 65 bytes long
+CERVER_PUBLIC void sha_256_generate (
+	char *output, const char *input, const size_t inlen
+);
+
+// generates a sha256 string from the input
+// returns a newly allocated string
+CERVER_PUBLIC char *sha_256_generate_output (
+	const char *input, const size_t inlen
+);
 
 #endif
