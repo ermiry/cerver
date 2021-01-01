42f813b179129d3be491d7de68b38a38f076ad90
ermiry <erick.salas@ermiry.com>
2020-12-31 23:50:25 -0600

    Updated sha256 implementation & refactored sources names

---
+++ include/cerver/utils/sha256.h
@@ -0,0 +1,31 @@
+#ifndef _CERVER_UTILS_SHA256_H_
+#define _CERVER_UTILS_SHA256_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "cerver/config.h"
+
+#define SHA256_STRING_LEN		80
+
+CERVER_PUBLIC void sha256_calc (
+	uint8_t hash[32], const void *input, size_t len
+);
+
+CERVER_PUBLIC void sha256_hash_to_string (
+	char string[], const uint8_t hash[32]
+);
+
+// generates a sha256 string from the input and places it in output
+// output must be at least 65 bytes long
+CERVER_PUBLIC void sha256_generate (
+	char *output, const char *input, const size_t inlen
+);
+
+// generates a sha256 string from the input
+// returns a newly allocated string
+CERVER_PUBLIC char *sha256_generate_output (
+	const char *input, const size_t inlen
+);
+
+#endif
--- src/cerver/utils/sha256.c
+++ src/cerver/utils/sha256.c
@@ -1,19 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "cerver/utils/sha256.h"
+
 // Copyrigth 	Alain Mosnier, amosnier
 // https://github.com/amosnier/sha-2
 
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-
-#include "cerver/utils/sha-256.h"
-
-#define CHUNK_SIZE 			64
-#define TOTAL_LEN_LEN 		8
-
-/*
- * Comments from pseudo-code at https://en.wikipedia.org/wiki/SHA-2 are reproduced here.
- * When useful for clarification, portions of the pseudo-code are reproduced here too.
- */
+#define CHUNK_SIZE			64
+#define TOTAL_LEN_LEN		8
 
 /*
  * Initialize array of round constants:
@@ -32,7 +28,7 @@ static const uint32_t k[] = {
 
 struct buffer_state {
 
-	const uint8_t * p;
+	const uint8_t *p;
 	size_t len;
 	size_t total_len;
 	int single_one_delivered; /* bool */
@@ -40,19 +36,17 @@ struct buffer_state {
 
 };
 
-/*
-* Defined behaviour in standard C for all count where 0 < count < 32,
-* which is what we need here.
-*/
 static inline uint32_t right_rot (uint32_t value, unsigned int count) {
-
+	
 	return value >> count | value << (32 - count);
 
 }
 
-static void init_buf_state (struct buffer_state *state, const void *input, size_t len) {
+static void init_buf_state (
+	struct buffer_state *state, const void * input, size_t len
+) {
 
-	state->p = (uint8_t *) input;
+	state->p = input;
 	state->len = len;
 	state->total_len = len;
 	state->single_one_delivered = 0;
@@ -61,22 +55,24 @@ static void init_buf_state (struct buffer_state *state, const void *input, size_
 }
 
 /* Return value: bool */
-static int calc_chunk (uint8_t chunk[CHUNK_SIZE], struct buffer_state * state) {
+static int calc_chunk (
+	uint8_t chunk[CHUNK_SIZE], struct buffer_state *state
+) {
 
-	size_t space_in_chunk = 0;
+	size_t space_in_chunk;
 
 	if (state->total_len_delivered) {
 		return 0;
 	}
 
 	if (state->len >= CHUNK_SIZE) {
-		memcpy (chunk, state->p, CHUNK_SIZE);
+		(void) memcpy (chunk, state->p, CHUNK_SIZE);
 		state->p += CHUNK_SIZE;
 		state->len -= CHUNK_SIZE;
 		return 1;
 	}
 
-	memcpy (chunk, state->p, state->len);
+	(void) memcpy (chunk, state->p, state->len);
 	chunk += state->len;
 	space_in_chunk = CHUNK_SIZE - state->len;
 	state->p += state->len;
@@ -99,7 +95,7 @@ static int calc_chunk (uint8_t chunk[CHUNK_SIZE], struct buffer_state * state) {
 		const size_t left = space_in_chunk - TOTAL_LEN_LEN;
 		size_t len = state->total_len;
 		int i;
-		memset (chunk, 0x00, left);
+		(void) memset(chunk, 0x00, left);
 		chunk += left;
 
 		/* Storing of len * 8 as a big endian 64-bit without overflow. */
@@ -110,8 +106,10 @@ static int calc_chunk (uint8_t chunk[CHUNK_SIZE], struct buffer_state * state) {
 			len >>= 8;
 		}
 		state->total_len_delivered = 1;
-	} else {
-		memset(chunk, 0x00, space_in_chunk);
+	}
+	
+	else {
+		(void) memset (chunk, 0x00, space_in_chunk);
 	}
 
 	return 1;
@@ -125,8 +123,9 @@ static int calc_chunk (uint8_t chunk[CHUNK_SIZE], struct buffer_state * state) {
  *   for bit string lengths that are not multiples of eight, and it really operates on arrays of bytes.
  *   In particular, the len parameter is a number of bytes.
  */
-void sha_256_calc (uint8_t hash[32], const void * input, size_t len) {
-
+void sha256_calc (
+	uint8_t hash[32], const void * input, size_t len
+) {
 	/*
 	 * Note 1: All integers (expect indexes) are 32-bit unsigned integers and addition is calculated modulo 2^32.
 	 * Note 2: For each round, there is one round constant k[i] and one entry in the message schedule array w[i], 0 = i = 63
@@ -140,62 +139,77 @@ void sha_256_calc (uint8_t hash[32], const void * input, size_t len) {
 	 * Initialize hash values:
 	 * (first 32 bits of the fractional parts of the square roots of the first 8 primes 2..19):
 	 */
-	uint32_t h[] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
-	int i = 0, j = 0;
+	uint32_t h[] = {
+		0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+	};
+
+	unsigned i, j;
 
 	/* 512-bit chunks is what we will operate on. */
-	uint8_t chunk[64] = { 0 };
+	uint8_t chunk[64];
 
 	struct buffer_state state = { 0 };
 
 	init_buf_state (&state, input, len);
 
 	while (calc_chunk (chunk, &state)) {
-		uint32_t ah[8] = { 0 };
+		uint32_t ah[8];
 
-		/*
-		 * create a 64-entry message schedule array w[0..63] of 32-bit words
-		 * (The initial values in w[0..63] don't matter, so many implementations zero them here)
-		 * copy chunk into first 16 words w[0..15] of the message schedule array
-		 */
-		uint32_t w[64];
 		const uint8_t *p = chunk;
-
-		memset (w, 0x00, sizeof w);
-		for (i = 0; i < 16; i++) {
-			w[i] = (uint32_t) p[0] << 24 | (uint32_t) p[1] << 16 |
-				(uint32_t) p[2] << 8 | (uint32_t) p[3];
-			p += 4;
-		}
-
-		/* Extend the first 16 words into the remaining 48 words w[16..63] of the message schedule array: */
-		for (i = 16; i < 64; i++) {
-			const uint32_t s0 = right_rot (w[i - 15], 7) ^ right_rot (w[i - 15], 18) ^ (w[i - 15] >> 3);
-			const uint32_t s1 = right_rot (w[i - 2], 17) ^ right_rot (w[i - 2], 19) ^ (w[i - 2] >> 10);
-			w[i] = w[i - 16] + s0 + w[i - 7] + s1;
-		}
 
 		/* Initialize working variables to current hash value: */
 		for (i = 0; i < 8; i++)
 			ah[i] = h[i];
 
 		/* Compression function main loop: */
-		for (i = 0; i < 64; i++) {
-			const uint32_t s1 = right_rot (ah[4], 6) ^ right_rot (ah[4], 11) ^ right_rot (ah[4], 25);
-			const uint32_t ch = (ah[4] & ah[5]) ^ (~ah[4] & ah[6]);
-			const uint32_t temp1 = ah[7] + s1 + ch + k[i] + w[i];
-			const uint32_t s0 = right_rot (ah[0], 2) ^ right_rot (ah[0], 13) ^ right_rot (ah[0], 22);
-			const uint32_t maj = (ah[0] & ah[1]) ^ (ah[0] & ah[2]) ^ (ah[1] & ah[2]);
-			const uint32_t temp2 = s0 + maj;
+		for (i = 0; i < 4; i++) {
+			/*
+			 * The w-array is really w[64], but since we only need
+			 * 16 of them at a time, we save stack by calculating
+			 * 16 at a time.
+			 *
+			 * This optimization was not there initially and the
+			 * rest of the comments about w[64] are kept in their
+			 * initial state.
+			 */
 
-			ah[7] = ah[6];
-			ah[6] = ah[5];
-			ah[5] = ah[4];
-			ah[4] = ah[3] + temp1;
-			ah[3] = ah[2];
-			ah[2] = ah[1];
-			ah[1] = ah[0];
-			ah[0] = temp1 + temp2;
+			/*
+			 * create a 64-entry message schedule array w[0..63] of 32-bit words
+			 * (The initial values in w[0..63] don't matter, so many implementations zero them here)
+			 * copy chunk into first 16 words w[0..15] of the message schedule array
+			 */
+			uint32_t w[16];
+
+			for (j = 0; j < 16; j++) {
+				if (i == 0) {
+					w[j] = (uint32_t) p[0] << 24 | (uint32_t) p[1] << 16 |
+						(uint32_t) p[2] << 8 | (uint32_t) p[3];
+					p += 4;
+				}
+				
+				else {
+					/* Extend the first 16 words into the remaining 48 words w[16..63] of the message schedule array: */
+					const uint32_t s0 = right_rot (w[(j + 1) & 0xf], 7) ^ right_rot (w[(j + 1) & 0xf], 18) ^ (w[(j + 1) & 0xf] >> 3);
+					const uint32_t s1 = right_rot (w[(j + 14) & 0xf], 17) ^ right_rot (w[(j + 14) & 0xf], 19) ^ (w[(j + 14) & 0xf] >> 10);
+					w[j] = w[j] + s0 + w[(j + 9) & 0xf] + s1;
+				}
+
+				const uint32_t s1 = right_rot (ah[4], 6) ^ right_rot (ah[4], 11) ^ right_rot (ah[4], 25);
+				const uint32_t ch = (ah[4] & ah[5]) ^ (~ah[4] & ah[6]);
+				const uint32_t temp1 = ah[7] + s1 + ch + k[i << 4 | j] + w[j];
+				const uint32_t s0 = right_rot (ah[0], 2) ^ right_rot (ah[0], 13) ^ right_rot (ah[0], 22);
+				const uint32_t maj = (ah[0] & ah[1]) ^ (ah[0] & ah[2]) ^ (ah[1] & ah[2]);
+				const uint32_t temp2 = s0 + maj;
+
+				ah[7] = ah[6];
+				ah[6] = ah[5];
+				ah[5] = ah[4];
+				ah[4] = ah[3] + temp1;
+				ah[3] = ah[2];
+				ah[2] = ah[1];
+				ah[1] = ah[0];
+				ah[0] = temp1 + temp2;
+			}
 		}
 
 		/* Add the compressed chunk to the current hash value: */
@@ -213,10 +227,42 @@ void sha_256_calc (uint8_t hash[32], const void * input, size_t len) {
 
 }
 
-void sha_256_hash_to_string (char string[65], const uint8_t hash[32]) {
+void sha256_hash_to_string (
+	char string[65], const uint8_t hash[32]
+) {
 
-	size_t i;
-	for (i = 0; i < 32; i++)
-		string += sprintf(string, "%02x", hash[i]);
+	for (size_t i = 0; i < 32; i++)
+		string += sprintf (string, "%02x", hash[i]);
+
+}
+
+// generates a sha256 string from the input and places it in output
+// output must be at least 65 bytes long
+void sha256_generate (
+	char *output, const char *input, const size_t inlen
+) {
+
+	uint8_t hash[32] = { 0 };
+
+	sha256_calc (hash, input, inlen);
+	sha256_hash_to_string (output, hash);
+
+}
+
+// generates a sha256 string from the input
+// returns a newly allocated string
+char *sha256_generate_output (
+	const char *input, const size_t inlen
+) {
+
+	char *output = (char *) calloc (SHA256_STRING_LEN, sizeof (char));
+	if (output) {
+		uint8_t hash[32] = { 0 };
+
+		sha256_calc (hash, input, inlen);
+		sha256_hash_to_string (output, hash);
+	}
+
+	return output;
 
 }
