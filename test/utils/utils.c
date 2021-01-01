8d52d4a250833689ac30850ad17eeb29106263a1
ermiry <erick.salas@ermiry.com>
2020-12-31 23:52:55 -0600

    Added more unit tests to check utils methods

---
+++ test/utils/utils.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "utils.h"
+
+int main (int argc, char **argv) {
+
+	(void) printf ("Testing UTILS...\n");
+
+	utils_tests_base64 ();
+
+	utils_tests_c_strings ();
+
+	utils_tests_sha256 ();
+
+	(void) printf ("\nDone with UTILS tests!\n\n");
+
+	return 0;
+
+}
