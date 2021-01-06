74550d1e04a9da75e0a53adaf6d03109231a2093
ermiry <erick.salas@ermiry.com>
2021-01-06 10:43:39 -0600

    Added missing steps in coverage script

--- coverage.sh
+++ coverage.sh
@@ -9,10 +9,12 @@ make TYPE=test -j8
 printf "\n\ngcc examples\n\n"
 make TYPE=test examples -j8
 printf "\n\ngcc test\n\n"
-make test -j8
+make TYPE=test test -j8
 
 # run
 bash test/run.sh
+
+make coverage
 
 # clean up
 make clean
