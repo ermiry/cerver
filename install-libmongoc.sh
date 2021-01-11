68392cc467c6ccd1931c225a7e012a9b6db3023a
ermiry <erick.salas@ermiry.com>
2021-01-11 13:27:24 -0600

    Updated libmongoc installation instructions

--- install-libmongoc.sh
+++ install-libmongoc.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+MONGOC_VERSION=1.17.3
+
 sudo apt-get update && sudo apt-get install -y cmake wget
 
-wget https://github.com/mongodb/mongo-c-driver/releases/download/1.15.1/mongo-c-driver-1.15.1.tar.gz
-tar xzf mongo-c-driver-1.15.1.tar.gz
+sudo mkdir /opt/mongoc
+cd /opt/mongoc
 
-cd mongo-c-driver-1.15.1
-mkdir cmake-build
-cd cmake-build
-cmake -D ENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF -D CMAKE_BUILD_TYPE=Release ..
-sudo make -j8 && sudo make install && sudo ldconfig
+sudo wget -q https://github.com/mongodb/mongo-c-driver/releases/download/$MONGOC_VERSION/mongo-c-driver-$MONGOC_VERSION.tar.gz
+sudo tar xzf mongo-c-driver-$MONGOC_VERSION.tar.gz
+
+cd mongo-c-driver-$MONGOC_VERSION
+sudo mkdir cmake-build && cd cmake-build
+sudo cmake -D ENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF -D CMAKE_BUILD_TYPE=Release -D ENABLE_SSL=OPENSSL -D ENABLE_SRV=ON ..
+sudo make -j4 && sudo make install
