57f77dadf7fb46d4da4e78f267d9e6b7fcb923b1
ermiry <erick.salas@ermiry.com>
2021-01-11 13:25:59 -0600

    Enabling NATIVE option when performing benchmarks

--- bench.sh
+++ bench.sh
@@ -4,13 +4,13 @@
 make clean
 
 printf "sources\n\n"
-make TYPE=test -j8
+make TYPE=test NATIVE=1 -j8
 
 printf "\n\nexamples\n\n"
-make TYPE=test examples -j8
+make TYPE=test NATIVE=1 examples -j8
 
 printf "\n\ntest\n\n"
-make TYPE=test test -j8
+make TYPE=test NATIVE=1 test -j8
 
 printf "\n\nbench\n\n"
-make TYPE=test bench -j8
+make TYPE=test NATIVE=1 bench -j8
