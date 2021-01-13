58360a2e546e7b77ca4fb15c1315a226b40bb50d
ermiry <erick.salas@ermiry.com>
2021-01-12 22:23:28 -0600

    Refactored run tests scripts to exit on any error

--- test/run.sh
+++ test/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH=bin ./test/bin/collections --quiet
+LD_LIBRARY_PATH=bin ./test/bin/collections --quiet || { exit 1; }
 
-LD_LIBRARY_PATH=bin ./test/bin/utils
+LD_LIBRARY_PATH=bin ./test/bin/utils || { exit 1; }
 
-LD_LIBRARY_PATH=bin ./test/bin/json
+LD_LIBRARY_PATH=bin ./test/bin/http || { exit 1; }
 
-LD_LIBRARY_PATH=bin ./test/bin/jwt
+LD_LIBRARY_PATH=bin ./test/bin/json || { exit 1; }
+
+LD_LIBRARY_PATH=bin ./test/bin/jwt || { exit 1; }
