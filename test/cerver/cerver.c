3c6561de9c051ab141418c9e6bc5e870cc926fa7
ermiry <erick.salas@ermiry.com>
2021-01-16 17:14:04 -0600

    Added more cerver events test callback methods in common sources

--- test/cerver/cerver.c
+++ test/cerver/cerver.c
@@ -5,8 +5,6 @@
 #include <cerver/cerver.h>
 #include <cerver/events.h>
 
-#include <app/app.h>
-
 #include "../test.h"
 
 void *on_cever_started (void *event_data_ptr) {
@@ -18,8 +16,10 @@ void *on_cever_started (void *event_data_ptr) {
 	test_check_ptr (event_data->cerver);
 	test_check_ptr (event_data->cerver->info);
 
+	test_check_null_ptr (event_data->client);
+	test_check_null_ptr (event_data->connection);
+
 	test_check_ptr (event_data->action_args);
-	free (event_data->action_args);
 
 	cerver_event_data_delete (event_data);
 
@@ -35,6 +35,9 @@ void *on_cever_teardown (void *event_data_ptr) {
 
 	test_check_ptr (event_data->cerver);
 	test_check_ptr (event_data->cerver->info);
+
+	test_check_null_ptr (event_data->client);
+	test_check_null_ptr (event_data->connection);
 
 	cerver_event_data_delete (event_data);
 
@@ -62,6 +65,26 @@ void *on_client_connected (void *event_data_ptr) {
 
 }
 
+void *on_client_new_connection (void *event_data_ptr) {
+
+	test_check_ptr (event_data_ptr);
+
+	CerverEventData *event_data = (CerverEventData *) event_data_ptr;
+
+	test_check_ptr (event_data->cerver);
+	test_check_ptr (event_data->cerver->info);
+
+	test_check_ptr (event_data->client);
+	test_check_ptr (event_data->connection);
+	test_check_ptr (event_data->connection->socket);
+	test_check_int_ne (event_data->connection->socket->sock_fd, 0);
+
+	cerver_event_data_delete (event_data);
+
+	return NULL;
+
+}
+
 void *on_client_close_connection (void *event_data_ptr) {
 
 	test_check_ptr (event_data_ptr);
@@ -70,6 +93,111 @@ void *on_client_close_connection (void *event_data_ptr) {
 
 	test_check_ptr (event_data->cerver);
 	test_check_ptr (event_data->cerver->info);
+
+	test_check_null_ptr (event_data->client);
+	test_check_null_ptr (event_data->connection);
+
+	cerver_event_data_delete (event_data);
+
+	return NULL;
+
+}
+
+void *on_hold_connected (void *event_data_ptr) {
+
+	test_check_ptr (event_data_ptr);
+
+	CerverEventData *event_data = (CerverEventData *) event_data_ptr;
+
+	test_check_ptr (event_data->cerver);
+	test_check_ptr (event_data->cerver->info);
+
+	test_check_null_ptr (event_data->client);
+
+	test_check_ptr (event_data->connection);
+	test_check_ptr (event_data->connection->socket);
+	test_check_int_ne (event_data->connection->socket->sock_fd, 0);
+	test_check_int_ne (event_data->connection->socket->sock_fd, -1);
+
+	cerver_event_data_delete (event_data);
+
+	return NULL;
+
+}
+
+void *on_hold_disconnected (void *event_data_ptr) {
+
+	test_check_ptr (event_data_ptr);
+
+	CerverEventData *event_data = (CerverEventData *) event_data_ptr;
+
+	test_check_ptr (event_data->cerver);
+	test_check_ptr (event_data->cerver->info);
+
+	test_check_null_ptr (event_data->client);
+	test_check_null_ptr (event_data->connection);
+
+	cerver_event_data_delete (event_data);
+
+	return NULL;
+
+}
+
+void *on_hold_drop (void *event_data_ptr) {
+
+	test_check_ptr (event_data_ptr);
+
+	CerverEventData *event_data = (CerverEventData *) event_data_ptr;
+
+	test_check_ptr (event_data->cerver);
+	test_check_ptr (event_data->cerver->info);
+
+	test_check_null_ptr (event_data->client);
+	test_check_null_ptr (event_data->connection);
+
+	cerver_event_data_delete (event_data);
+
+	return NULL;
+
+}
+
+void *on_client_success_auth (void *event_data_ptr) {
+
+	test_check_ptr (event_data_ptr);
+
+	CerverEventData *event_data = (CerverEventData *) event_data_ptr;
+
+	test_check_ptr (event_data->cerver);
+	test_check_ptr (event_data->cerver->info);
+
+	test_check_ptr (event_data->client);
+
+	test_check_ptr (event_data->connection);
+	test_check_ptr (event_data->connection->socket);
+	test_check_int_ne (event_data->connection->socket->sock_fd, 0);
+	test_check_int_ne (event_data->connection->socket->sock_fd, -1);
+
+	cerver_event_data_delete (event_data);
+
+	return NULL;
+
+}
+
+void *on_client_failed_auth (void *event_data_ptr) {
+
+	test_check_ptr (event_data_ptr);
+
+	CerverEventData *event_data = (CerverEventData *) event_data_ptr;
+
+	test_check_ptr (event_data->cerver);
+	test_check_ptr (event_data->cerver->info);
+
+	test_check_null_ptr (event_data->client);
+
+	test_check_ptr (event_data->connection);
+	test_check_ptr (event_data->connection->socket);
+	test_check_int_ne (event_data->connection->socket->sock_fd, 0);
+	test_check_int_ne (event_data->connection->socket->sock_fd, -1);
 
 	cerver_event_data_delete (event_data);
 
--- test/cerver/cerver.h
+++ test/cerver/cerver.h
@@ -7,6 +7,18 @@ extern void *on_cever_teardown (void *event_data_ptr);
 
 extern void *on_client_connected (void *event_data_ptr);
 
+extern void *on_client_new_connection (void *event_data_ptr);
+
 extern void *on_client_close_connection (void *event_data_ptr);
+
+extern void *on_hold_connected (void *event_data_ptr);
+
+extern void *on_hold_disconnected (void *event_data_ptr);
+
+extern void *on_hold_drop (void *event_data_ptr);
+
+extern void *on_client_success_auth (void *event_data_ptr);
+
+extern void *on_client_failed_auth (void *event_data_ptr);
 
 #endif
