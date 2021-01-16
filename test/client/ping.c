e6ec7381ab136b40f945f73e95b90cd73dc445a2
ermiry <erick.salas@ermiry.com>
2021-01-15 23:39:20 -0600

    Refactored client ping test to use check macros

--- test/client/ping.c
+++ test/client/ping.c
@@ -8,44 +8,45 @@
 
 int main (int argc, const char **argv) {
 
-	(void) printf ("Ping test\n");
+	(void) printf ("Testing CLIENT ping...\n");
 
 	Client *client = client_create ();
-	if (client) {
-		client_set_name (client, "test-client");
 
-		// Handler *app_handler = handler_create (client_app_handler);
-		// handler_set_direct_handle (app_handler, true);
-		// client_set_app_handlers (client, app_handler, NULL);
+	test_check_ptr (client);
 
-		Connection *connection = client_connection_create (
-			client, "127.0.0.1", 7000, PROTOCOL_TCP, false
-		);
+	client_set_name (client, "test-client");
 
-		if (connection) {
-			connection_set_max_sleep (connection, 30);
+	// Handler *app_handler = handler_create (client_app_handler);
+	// handler_set_direct_handle (app_handler, true);
+	// client_set_app_handlers (client, app_handler, NULL);
 
-			test_check_int_eq (
-				client_connect_and_start (client, connection), 0,
-				"Failed to connect to cerver!"
+	Connection *connection = client_connection_create (
+		client, "127.0.0.1", 7000, PROTOCOL_TCP, false
+	);
+
+	test_check_ptr (connection);
+
+	connection_set_max_sleep (connection, 30);
+
+	test_check_int_eq (
+		client_connect_and_start (client, connection), 0,
+		"Failed to connect to cerver!"
+	);
+
+	// send 10 requests message every second for 5 seconds
+	for (unsigned int sec = 0; sec < 5; sec++) {
+		for (unsigned int i = 0; i < 10; i++) {
+			test_check_unsigned_eq (
+				packet_send_ping (NULL, client, connection, NULL), 0, NULL
 			);
 		}
 
-		// send 10 requests message every second for 5 seconds
-		for (unsigned int sec = 0; sec < 5; sec++) {
-			for (unsigned int i = 0; i < 10; i++) {
-				test_check_unsigned_eq (
-					packet_send_ping (NULL, client, connection, NULL), 0, NULL
-				);
-			}
-
-			(void) sleep (1);
-		}
-
-		client_connection_end (client, connection);
-
-		client_teardown (client);
+		(void) sleep (1);
 	}
+
+	client_connection_end (client, connection);
+
+	client_teardown (client);
 
 	(void) printf ("Done!\n\n");
 
