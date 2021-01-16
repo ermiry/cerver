0616870d728737afc1aad038c0dc24726d832a01
ermiry <erick.salas@ermiry.com>
2021-01-15 23:41:58 -0600

    Updated run tests scripts to handle threads unit tests

--- test/run.sh
+++ test/run.sh
@@ -4,4 +4,6 @@ LD_LIBRARY_PATH=bin ./test/bin/cerver || { exit 1; }
 
 LD_LIBRARY_PATH=bin ./test/bin/collections --quiet || { exit 1; }
 
+LD_LIBRARY_PATH=bin ./test/bin/threads || { exit 1; }
+
 LD_LIBRARY_PATH=bin ./test/bin/utils || { exit 1; }
