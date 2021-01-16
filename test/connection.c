3cc7166551ccdce0387ac62e987563303551cc6b
ermiry <erick.salas@ermiry.com>
2021-01-15 23:33:07 -0600

    Added base connection methods unit tests

---
+++ test/connection.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include <cerver/connection.h>
+
+#include "test.h"
+
+int main (int argc, char **argv) {
+
+	(void) printf ("Testing CONNECTION...\n");
+
+	(void) printf ("\nDone with CONNECTION tests!\n\n");
+
+	return 0;
+
+}
