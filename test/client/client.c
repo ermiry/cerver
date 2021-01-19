afd8b980a91c8e53c684b759f944fd9c6d97be7c
ermiry <erick.salas@ermiry.com>
2021-01-18 23:31:01 -0600

    Added latest cerver & client unit & integration tests

---
+++ test/client/client.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <cerver/client.h>
+
+#include "../test.h"
+
+#pragma region events
+
+void *client_event_connection_close (void *client_event_data_ptr) {
+
+	test_check_ptr (client_event_data_ptr);
+
+	ClientEventData *client_event_data = (ClientEventData *) client_event_data_ptr;
+
+	test_check_ptr (client_event_data->client);
+
+	test_check_ptr (client_event_data->connection);
+	test_check_ptr (client_event_data->connection->name);
+	test_check_ptr (client_event_data->connection->name->str);
+
+	client_event_data_delete (client_event_data);
+
+	return NULL;
+
+}
+
+void *client_event_auth_sent (void *client_event_data_ptr) {
+
+	test_check_ptr (client_event_data_ptr);
+
+	ClientEventData *client_event_data = (ClientEventData *) client_event_data_ptr;
+
+	test_check_ptr (client_event_data->client);
+
+	test_check_ptr (client_event_data->connection);
+	test_check_ptr (client_event_data->connection->name);
+	test_check_ptr (client_event_data->connection->name->str);
+
+	client_event_data_delete (client_event_data);
+
+	return NULL;
+
+}
+
+void *client_event_success_auth (void *client_event_data_ptr) {
+
+	test_check_ptr (client_event_data_ptr);
+
+	ClientEventData *client_event_data = (ClientEventData *) client_event_data_ptr;
+
+	// test_check_null_ptr (client_event_data->client);
+
+	test_check_ptr (client_event_data->connection);
+	test_check_ptr (client_event_data->connection->name);
+	test_check_ptr (client_event_data->connection->name->str);
+
+	client_event_data_delete (client_event_data);
+
+	return NULL;
+
+}
+
+#pragma endregion
+
+#pragma region errors
+
+void *client_error_failed_auth (void *client_error_data_ptr) {
+
+	test_check_ptr (client_error_data_ptr);
+
+	ClientErrorData *client_error_data = (ClientErrorData *) client_error_data_ptr;
+
+	test_check_ptr (client_error_data->client);
+
+	test_check_ptr (client_error_data->connection);
+	test_check_ptr (client_error_data->connection->name);
+	test_check_ptr (client_error_data->connection->name->str);
+
+	client_error_data_delete (client_error_data);
+
+	return NULL;
+
+}
+
+#pragma endregion
---
+++ test/client/test.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include <time.h>
+
+#include <cerver/client.h>
+
+#include "../test.h"
+
+static const char *client_name = "test-client";
+
+static Client *test_client_create (void) {
+
+	Client *client = client_create ();
+
+	test_check_str_eq (client->name->str, "no-name", NULL);
+	test_check_str_len (client->name->str, strlen ("no-name"), NULL);
+	test_check_unsigned_ne (client->connected_timestamp, 0);
+	test_check_ptr (client->connections);
+	test_check_ptr (client->lock);
+	test_check_ptr (client->file_stats);
+	test_check_ptr (client->stats);
+
+	return client;
+
+}
+
+static void test_client_base_configuration (void) {
+
+	Client *client = test_client_create ();
+
+	client_set_name (client,client_name);
+	test_check_str_eq (client->name->str, client_name, NULL);
+	test_check_str_len (client->name->str, strlen (client_name), NULL);
+
+	client_delete (client);
+
+}
+
+int main (int argc, char **argv) {
+
+	srand ((unsigned) time (NULL));
+
+	(void) printf ("Testing CLIENT...\n");
+
+	test_client_base_configuration ();
+
+	(void) printf ("\nDone with CLIENT tests!\n\n");
+
+	return 0;
+
+}
