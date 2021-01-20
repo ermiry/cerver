a5845d15d1b5bb8b617014c8753bdb8293d911ee
ermiry <erick.salas@ermiry.com>
2021-01-19 22:41:48 -0600

    Removed receive handle definitions from handler header

--- include/cerver/handler.h
+++ include/cerver/handler.h
@@ -217,61 +217,6 @@ CERVER_PRIVATE void sock_receive_delete (void *sock_receive_ptr);
 // the default timeout when handling a connection in dedicated thread
 #define CERVER_DEFAULT_SOCKET_RECV_TIMEOUT         5
 
-#define RECEIVE_HANDLE_STATE_MAP(XX)			\
-	XX(0,	NONE, 			None)				\
-	XX(1,	NORMAL, 		Normal)				\
-	XX(2,	SPLIT_HEADER, 	Split-Header)		\
-	XX(3,	SPLIT_PACKET, 	Split-Packet)		\
-	XX(4,	COMP_HEADER, 	Complete-Header)	\
-	XX(5,	LOST, 			Lost)
-
-typedef enum ReceiveHandleState {
-
-	#define XX(num, name, string) RECEIVE_HANDLE_STATE_##name = num,
-	RECEIVE_HANDLE_STATE_MAP (XX)
-	#undef XX
-
-} ReceiveHandleState;
-
-CERVER_PUBLIC const char *receive_handle_state_to_string (
-	ReceiveHandleState state
-);
-
-struct _ReceiveHandle {
-
-	ReceiveType type;
-
-	struct _Cerver *cerver;
-
-	struct _Socket *socket;
-	struct _Connection *connection;
-	struct _Client *client;
-	struct _Admin *admin;
-
-	struct _Lobby *lobby;
-
-	char *buffer;
-	size_t buffer_size;
-	size_t received_size;
-
-	ReceiveHandleState state;
-
-	// used to handle split headers between buffers
-	// this can happen when the header is at the buffer's end
-	PacketHeader header;	
-	char *header_end;
-	unsigned int remaining_header;
-
-	struct _Packet *spare_packet;
-
-};
-
-typedef struct _ReceiveHandle ReceiveHandle;
-
-CERVER_PRIVATE ReceiveHandle *receive_handle_new (void);
-
-CERVER_PRIVATE void receive_handle_delete (void *receive_ptr);
-
 // default cerver receive handler
 CERVER_PRIVATE void cerver_receive_handle_buffer (
 	void *receive_handle_ptr
