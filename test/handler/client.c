8382ccde677a5c7dee312c07cf11d2f1b0a171a8
ermiry <erick.salas@ermiry.com>
2021-01-19 22:49:09 -0600

    Sending text messages from the handler's client test

--- test/handler/client.c
+++ test/handler/client.c
@@ -6,14 +6,86 @@
 
 #include <cerver/utils/log.h>
 
+#include <app/app.h>
+
+// static const char *MESSAGE = {
+// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+// 	"incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
+// 	"exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." 
+// };
+
+static const char *MESSAGE = {
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+	"incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
+	"exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+	"incididunt ut labore et dolore magna aliqua."
+};
+
+static Client *client = NULL;
+static Connection *connection = NULL;
+
+static unsigned int responses = 0;
+
+static void app_handler (void *packet_ptr) {
+
+	if (packet_ptr) {
+		Packet *packet = (Packet *) packet_ptr;
+
+		switch (packet->header.request_type) {
+			case APP_REQUEST_NONE: break;
+
+			case APP_REQUEST_TEST:
+				responses += 1;
+				break;
+
+			case APP_REQUEST_MESSAGE: break;
+
+			case APP_REQUEST_MULTI: break;
+
+			default: break;
+		}
+	}
+
+}
+
+static void single_app_message (const char *msg) {
+
+	Packet *message = packet_create (
+		PACKET_TYPE_APP, APP_REQUEST_MESSAGE,
+		msg, strlen (msg)
+	);
+
+	message->header.handler_id = 10;
+	message->header.sock_fd = 1023;
+
+	(void) packet_generate (message);
+
+	// send the packet
+	packet_set_network_values (
+		message, NULL, client, connection, NULL
+	);
+
+	size_t sent = 0;
+	(void) packet_send (message, 0, &sent, false);
+
+	packet_delete (message);
+
+}
+
 int main (int argc, const char **argv) {
 
 	cerver_log_init ();
 
-	Client *client = client_create ();
+	client = client_create ();
 	
 	client_set_name (client, "test-client");
-	Connection *connection = client_connection_create (
+
+	Handler *app_packet_handler = handler_create (app_handler);
+	handler_set_direct_handle (app_packet_handler, true);
+	client_set_app_handlers (client, app_packet_handler, NULL);
+
+	connection = client_connection_create (
 		client, "127.0.0.1", 7000, PROTOCOL_TCP, false
 	);
 
@@ -23,32 +95,26 @@ int main (int argc, const char **argv) {
 		unsigned int n_sent_packets = 0;
 
 		/*** send **/
-		for (unsigned int i = 0; i < 10; i++) {
-			if (!packet_send_ping (NULL, client, connection, NULL)) {
-				n_sent_packets += 1;	
-			}
-
-			else {
-				cerver_log_error ("Failed to send packet!");
-			}
+		for (size_t i = 0; i < 10000; i++) {
+			single_app_message (MESSAGE);
 		}
 
 		// wait for remaining packets
-		(void) sleep (2);
+		(void) sleep (4);
 
 		// check that we received matching responses
-		if (
-			client->stats->received_packets->n_test_packets
-			!= n_sent_packets
-		) {
-			(void) printf ("\n\n");
-			cerver_log_error (
-				"Responses %lu don't match n_sent_packets %lu!",
-				client->stats->received_packets->n_test_packets,
-				n_sent_packets
-			);
-			(void) printf ("\n\n");
-		}
+		// if (
+		// 	client->stats->received_packets->n_test_packets
+		// 	!= n_sent_packets
+		// ) {
+		// 	(void) printf ("\n\n");
+		// 	cerver_log_error (
+		// 		"Responses %lu don't match n_sent_packets %lu!",
+		// 		client->stats->received_packets->n_test_packets,
+		// 		n_sent_packets
+		// 	);
+		// 	(void) printf ("\n\n");
+		// }
 	}
 
 	else {
