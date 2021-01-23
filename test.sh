3688d4c6d08242e9f7aca85423f6e000b3c1ebb2
ermiry <erick.salas@ermiry.com>
2021-01-23 13:23:37 -0600

    Handling load balancer integration tests in dedicated test script

--- test.sh
+++ test.sh
@@ -102,3 +102,37 @@ sudo docker inspect test --format='{{.State.ExitCode}}' || { exit 1; }
 ./test/bin/client/threads || { exit 1; }
 
 sudo docker kill $(sudo docker ps -q)
+
+# load
+sudo docker network create cerver
+
+# service 1
+sudo docker run \
+	-d \
+	--name service-1 --rm \
+	-p 7001:7001 --net cerver \
+	ermiry/cerver:test ./bin/cerver/service -p 7001
+
+# service 2
+sudo docker run \
+	-d \
+	--name service-2 --rm \
+	-p 7002:7002 --net cerver \
+	ermiry/cerver:test ./bin/cerver/service -p 7002
+
+# balancer
+sudo docker run \
+	-d \
+	--name load --rm \
+	-p 7000:7000 --net cerver \
+	ermiry/cerver:test ./bin/cerver/load
+
+sleep 2
+
+sudo docker inspect load --format='{{.State.ExitCode}}' || { exit 1; }
+sudo docker inspect service-1 --format='{{.State.ExitCode}}' || { exit 1; }
+sudo docker inspect service-2 --format='{{.State.ExitCode}}' || { exit 1; }
+
+./test/bin/client/load || { exit 1; }
+
+sudo docker kill $(sudo docker ps -q)
