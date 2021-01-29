1f6b9de6d7f85556a0b3820e78e2f8fb64d52b65
ermiry <erick.salas@ermiry.com>
2021-01-28 22:44:13 -0600

    Refactored web client integration test internal methods

--- test/client/web.c
+++ test/client/web.c
@@ -6,6 +6,8 @@
 
 #include "curl.h"
 
+#define ADDRESS_SIZE			128
+
 static const char *address = { "localhost.com:8080" };
 
 static size_t web_request_all_data_handler (
@@ -25,7 +27,7 @@ static unsigned int web_request_all_actual (
 	unsigned int errors = 0;
 
 	char data_buffer[4096] = { 0 };
-	char actual_address[128] = { 0 };
+	char actual_address[ADDRESS_SIZE] = { 0 };
 
 	// GET /
 	errors |= curl_simple_handle_data (
@@ -34,56 +36,56 @@ static unsigned int web_request_all_actual (
 	);
 
 	// GET /test
-	(void) snprintf (actual_address, 128, "%s/test", address);
+	(void) snprintf (actual_address, ADDRESS_SIZE, "%s/test", address);
 	errors |= curl_simple_handle_data (
 		curl, actual_address,
 		web_request_all_data_handler, data_buffer
 	);
 
 	// GET /text
-	(void) snprintf (actual_address, 128, "%s/text", address);
+	(void) snprintf (actual_address, ADDRESS_SIZE, "%s/text", address);
 	errors |= curl_simple_handle_data (
 		curl, actual_address,
 		web_request_all_data_handler, data_buffer
 	);
 
 	// GET /json
-	(void) snprintf (actual_address, 128, "%s/json", address);
+	(void) snprintf (actual_address, ADDRESS_SIZE, "%s/json", address);
 	errors |= curl_simple_handle_data (
 		curl, actual_address,
 		web_request_all_data_handler, data_buffer
 	);
 
 	// GET /hola
-	(void) snprintf (actual_address, 128, "%s/hola", address);
+	(void) snprintf (actual_address, ADDRESS_SIZE, "%s/hola", address);
 	errors |= curl_simple_handle_data (
 		curl, actual_address,
 		web_request_all_data_handler, data_buffer
 	);
 
 	// GET /adios
-	(void) snprintf (actual_address, 128, "%s/adios", address);
+	(void) snprintf (actual_address, ADDRESS_SIZE, "%s/adios", address);
 	errors |= curl_simple_handle_data (
 		curl, actual_address,
 		web_request_all_data_handler, data_buffer
 	);
 
 	// GET /key
-	(void) snprintf (actual_address, 128, "%s/key", address);
+	(void) snprintf (actual_address, ADDRESS_SIZE, "%s/key", address);
 	errors |= curl_simple_handle_data (
 		curl, actual_address,
 		web_request_all_data_handler, data_buffer
 	);
 
 	// GET /custom
-	(void) snprintf (actual_address, 128, "%s/custom", address);
+	(void) snprintf (actual_address, ADDRESS_SIZE, "%s/custom", address);
 	errors |= curl_simple_handle_data (
 		curl, actual_address,
 		web_request_all_data_handler, data_buffer
 	);
 
 	// GET /reference
-	(void) snprintf (actual_address, 128, "%s/reference", address);
+	(void) snprintf (actual_address, ADDRESS_SIZE, "%s/reference", address);
 	errors |= curl_simple_handle_data (
 		curl, actual_address,
 		web_request_all_data_handler, data_buffer
