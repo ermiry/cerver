06ceaccd2dc7b04636770ee6b7d250111b169c31
ermiry <erick.salas@ermiry.com>
2021-01-29 17:08:57 -0600

    Added latest available cerver & client integration tests

---
+++ test/cerver/threads.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <time.h>
+#include <signal.h>
+
+#include <cerver/cerver.h>
+#include <cerver/events.h>
+#include <cerver/handler.h>
+
+#include <app/app.h>
+#include <app/handler.h>
+
+#include "cerver.h"
+#include "../test.h"
+
+static const char *cerver_name = "test-cerver";
+static const char *welcome_message = "Hello there!";
+
+static Cerver *cerver = NULL;
+
+static void end (int dummy) {
+	
+	cerver_teardown (cerver);
+
+	// cerver_end ();
+
+	exit (0);
+
+}
+
+int main (int argc, char **argv) {
+
+	srand ((unsigned int) time (NULL));
+
+	(void) signal (SIGINT, end);
+	(void) signal (SIGTERM, end);
+	(void) signal (SIGKILL, end);
+
+	cerver = cerver_create (
+		CERVER_TYPE_CUSTOM,
+		cerver_name,
+		CERVER_DEFAULT_PORT,
+		PROTOCOL_TCP,
+		false,
+		CERVER_DEFAULT_CONNECTION_QUEUE
+	);
+
+	test_check_ptr (cerver);
+	test_check_int_eq (cerver->type, CERVER_TYPE_CUSTOM, NULL);
+	test_check_ptr (cerver->info);
+	test_check_ptr (cerver->info->name);
+	test_check_str_eq (cerver->info->name->str, cerver_name, NULL);
+	test_check_str_len (cerver->info->name->str, strlen (cerver_name), NULL);
+	test_check_int_eq (cerver->port, CERVER_DEFAULT_PORT, NULL);
+	test_check_int_eq (cerver->protocol, PROTOCOL_TCP, NULL);
+	test_check_bool_eq (cerver->use_ipv6, false, NULL);
+	test_check_int_eq (cerver->connection_queue, CERVER_DEFAULT_CONNECTION_QUEUE, NULL);
+
+	cerver_set_welcome_msg (cerver, welcome_message);
+	test_check_ptr (cerver->info->welcome_msg);
+	test_check_str_eq (cerver->info->welcome_msg->str, welcome_message, NULL);
+	test_check_str_len (cerver->info->welcome_msg->str, strlen (welcome_message), NULL);
+
+	cerver_set_receive_buffer_size (cerver, 4096);
+	test_check_unsigned_eq (cerver->receive_buffer_size, 4096, NULL);
+
+	cerver_set_thpool_n_threads (cerver, 4);
+	test_check_unsigned_eq (cerver->n_thpool_threads, 4, NULL);
+
+	cerver_set_reusable_address_flags (cerver, true);
+	test_check_bool_eq (cerver->reusable, true, NULL);
+
+	cerver_set_handler_type (cerver, CERVER_HANDLER_TYPE_THREADS);
+	test_check_int_eq (cerver->handler_type, CERVER_HANDLER_TYPE_THREADS, NULL);
+
+	/*** handlers ***/
+	Handler *app_packet_handler = handler_create (app_handler);
+	handler_set_direct_handle (app_packet_handler, true);
+	cerver_set_app_handlers (cerver, app_packet_handler, NULL);
+
+	/*** events ***/
+	u8 event_result = 0;
+	event_result = cerver_event_register (
+		cerver, 
+		CERVER_EVENT_CLIENT_CONNECTED,
+		on_client_connected, NULL, NULL,
+		false, false
+	);
+
+	test_check_unsigned_eq (event_result, 0, NULL);
+
+	event_result = cerver_event_register (
+		cerver, 
+		CERVER_EVENT_CLIENT_CLOSE_CONNECTION,
+		on_client_close_connection, NULL, NULL,
+		false, false
+	);
+
+	test_check_unsigned_eq (event_result, 0, NULL);
+
+	/*** start ***/
+	test_check_unsigned_eq (
+		cerver_start (cerver), 0, "Failed to start cerver!"
+	);
+
+	return 0;
+
+}
---
+++ test/client/auth.c
@@ -0,0 +1,174 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <cerver/client.h>
+#include <cerver/packets.h>
+
+#include <app/app.h>
+#include <app/auth.h>
+
+#include "client.h"
+
+#include "../test.h"
+
+#define REQUESTS			64
+
+static const char *client_name = "test-client";
+static const char *connection_name = "test-connection";
+
+static unsigned int responses = 0;
+
+static void app_handler (void *packet_ptr) {
+
+	if (packet_ptr) {
+		Packet *packet = (Packet *) packet_ptr;
+
+		switch (packet->header.request_type) {
+			case APP_REQUEST_NONE: break;
+
+			case APP_REQUEST_TEST:
+				responses += 1;
+				break;
+
+			case APP_REQUEST_MESSAGE: break;
+
+			case APP_REQUEST_MULTI: break;
+
+			default: break;
+		}
+	}
+
+}
+
+int main (int argc, const char **argv) {
+
+	(void) printf ("Testing CLIENT auth...\n");
+
+	Client *client = client_create ();
+
+	test_check_ptr (client);
+
+	client_set_name (client, client_name);
+	test_check_str_eq (client->name, client_name, NULL);
+	test_check_str_len (client->name, strlen (client_name), NULL);
+
+	/*** handler ***/
+	Handler *app_packet_handler = handler_create (app_handler);
+	handler_set_direct_handle (app_packet_handler, true);
+	client_set_app_handlers (client, app_packet_handler, NULL);
+
+	/*** events ***/
+	u8 event_result = 0;
+
+	event_result = client_event_register (
+		client,
+		CLIENT_EVENT_CONNECTION_CLOSE,
+		client_event_connection_close, NULL, NULL,
+		false, false
+	);
+
+	test_check_unsigned_eq (event_result, 0, NULL);
+
+	event_result = client_event_register (
+		client,
+		CLIENT_EVENT_AUTH_SENT,
+		client_event_auth_sent, NULL, NULL,
+		true, false
+	);
+
+	test_check_unsigned_eq (event_result, 0, NULL);
+
+	event_result = client_event_register (
+		client,
+		CLIENT_EVENT_SUCCESS_AUTH,
+		client_event_success_auth, NULL, NULL,
+		false, false
+	);
+
+	test_check_unsigned_eq (event_result, 0, NULL);
+
+	/*** errors ***/
+	u8 error_result = 0;
+
+	error_result = client_error_register (
+		client,
+		CLIENT_ERROR_FAILED_AUTH,
+		client_error_failed_auth, NULL, NULL,
+		false, false
+	);
+
+	test_check_unsigned_eq (error_result, 0, NULL);
+
+	/*** connection ***/
+	Connection *connection = client_connection_create (
+		client, "127.0.0.1", 7000, PROTOCOL_TCP, false
+	);
+
+	test_check_ptr (connection);
+
+	connection_set_name (connection, connection_name);
+	connection_set_max_sleep (connection, 30);
+
+	Credentials *credentials = credentials_new (
+		"ermiry", "049ec1af7c1332193d602986f2fdad5b4d1c2ff90e5cdc65388c794c1f10226b"
+	);
+
+	connection_set_auth_data (
+		connection, 
+		credentials, sizeof (Credentials), 
+		credentials_delete,
+		false
+	);
+
+	/*** start ***/
+	test_check_int_eq (
+		client_connect_and_start (client, connection), 0,
+		"Failed to connect to cerver!"
+	);
+
+	// wait to be authenticated with cerver
+	(void) sleep (3);
+
+	test_check_bool_eq (
+		connection->authenticated, true, "Failed to authenticate!"
+	);
+
+	/*** send ***/
+	// send a bunch of requests to the cerver
+	Packet *request = NULL;
+	for (unsigned int i = 0; i < REQUESTS; i++) {
+		request = packet_create_request (
+			PACKET_TYPE_APP, APP_REQUEST_TEST
+		);
+
+		if (request) {
+			packet_set_network_values (
+				request,
+				NULL, client, connection, NULL
+			);
+
+			test_check_unsigned_eq (
+				packet_send (request, 0, NULL, false),
+				0, NULL
+			);
+
+			packet_delete (request);
+		}
+	}
+
+	// wait for any response to arrive
+	(void) sleep (2);
+
+	/*** check ***/
+	// check that we have received all the responses
+	test_check_unsigned_eq (responses, REQUESTS, NULL);
+
+	/*** end ***/
+	client_connection_end (client, connection);
+	client_teardown (client);
+
+	(void) printf ("Done!\n\n");
+
+	return 0;
+
+}
