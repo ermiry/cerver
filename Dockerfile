79c6391e374ae978a5620a56ea5424195ddb05f3
ermiry <erick.salas@ermiry.com>
2021-01-31 20:32:08 -0600

    Updated version to Beta 1.6.4b-5

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-ARG CERVER_VERSION=1.6.4b-4
+ARG CERVER_VERSION=1.6.4b-5
 
 FROM gcc as builder
 
--- cerver-install.sh
+++ cerver-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CERVER_VERSION=1.6.4b-4
+CERVER_VERSION=1.6.4b-5
 
 # sudo apt-get update && sudo apt-get install -y make wget
 
