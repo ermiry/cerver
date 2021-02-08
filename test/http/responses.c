ef941a2977cd471d60f97defb3f0ad3cb973b410
ermiry <erick.salas@ermiry.com>
2021-02-07 18:44:49 -0600

    Correctly testing compiled headers in responses unit tests

--- test/http/responses.c
+++ test/http/responses.c
@@ -2,14 +2,62 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <cerver/version.h>
+
+#include <cerver/http/http.h>
 #include <cerver/http/response.h>
 
 #include "../test.h"
 
-static const char *header = { "HTTP/1.1 200 OK\r\nServer: Cerver/2.0\r\nContent-Type: application/json\r\nContent-Length: 128\r\n\r\n" };
+#define TEST_HEADER_LEN		256
+
+static char header[TEST_HEADER_LEN] = { 0 };
+static char header_content_len[TEST_HEADER_LEN] = { 0 };
+static char header_content_type[TEST_HEADER_LEN] = { 0 };
+
 static const char *content_type_header = { "Content-Type: application/json\r\n" };
 static const char *content_length_header = { "Content-Length: 128\r\n" };
 static const char *json_data = { "{ \"oki\": \"doki\" }" };
+
+static HttpCerver *http_cerver = NULL;
+
+static void test_http_responses_init (void) {
+
+	(void) snprintf (
+		header, TEST_HEADER_LEN,
+		"HTTP/1.1 200 OK\r\nServer: Cerver/%s\r\nContent-Length: 128\r\nContent-Type: application/json\r\n\r\n",
+		CERVER_VERSION
+	);
+
+	(void) snprintf (
+		header_content_len, TEST_HEADER_LEN,
+		"HTTP/1.1 200 OK\r\nServer: Cerver/%s\r\nContent-Length: 128\r\n\r\n",
+		CERVER_VERSION
+	);
+
+	(void) snprintf (
+		header_content_type, TEST_HEADER_LEN,
+		"HTTP/1.1 200 OK\r\nServer: Cerver/%s\r\nContent-Type: application/json\r\n\r\n",
+		CERVER_VERSION
+	);
+
+	http_cerver = http_cerver_new ();
+
+	test_check_ptr (http_cerver);
+
+	test_check_unsigned_eq (
+		http_responses_init (http_cerver), 0, "Failed to init HTTP responses!"
+	);
+
+}
+
+static void test_http_responses_end (void) {
+
+	http_responses_end ();
+
+	http_cerver_delete (http_cerver);
+
+}
 
 static HttpResponse *test_http_response_new (void) {
 
@@ -126,8 +174,19 @@ static void test_http_response_add_header (void) {
 	test_check_null_ptr (response->header);
 	test_check_unsigned_eq (response->header_len, 0, NULL);
 
-	// TODO: compile header
-	// http_response_compile_header (response);
+	// compile header
+	http_response_compile_header (response);
+
+	test_check_ptr (response->header);
+	test_check_str_eq (response->header, header, NULL);
+	test_check_unsigned_eq (response->header_len, strlen (header), NULL);
+
+	test_check_null_ptr (response->data);
+	test_check_unsigned_eq (response->data_len, 0, NULL);
+	test_check_false (response->data_ref);
+
+	test_check_null_ptr (response->res);
+	test_check_unsigned_eq (response->res_len, 0, NULL);
 
 	http_response_delete (response);
 
@@ -151,6 +210,20 @@ static void test_http_response_add_content_type_header (void) {
 	test_check_null_ptr (response->header);
 	test_check_unsigned_eq (response->header_len, 0, NULL);
 
+	// compile header
+	http_response_compile_header (response);
+
+	test_check_ptr (response->header);
+	test_check_str_eq (response->header, header_content_type, NULL);
+	test_check_unsigned_eq (response->header_len, strlen (header_content_type), NULL);
+
+	test_check_null_ptr (response->data);
+	test_check_unsigned_eq (response->data_len, 0, NULL);
+	test_check_false (response->data_ref);
+
+	test_check_null_ptr (response->res);
+	test_check_unsigned_eq (response->res_len, 0, NULL);
+
 	http_response_delete (response);
 
 }
@@ -172,6 +245,20 @@ static void test_http_response_add_content_length_header (void) {
 	test_check_str_len (response->headers[HTTP_HEADER_CONTENT_LENGTH]->str, strlen (content_length_header), NULL);
 	test_check_null_ptr (response->header);
 	test_check_unsigned_eq (response->header_len, 0, NULL);
+
+	// compile header
+	http_response_compile_header (response);
+
+	test_check_ptr (response->header);
+	test_check_str_eq (response->header, header_content_len, NULL);
+	test_check_unsigned_eq (response->header_len, strlen (header_content_len), NULL);
+
+	test_check_null_ptr (response->data);
+	test_check_unsigned_eq (response->data_len, 0, NULL);
+	test_check_false (response->data_ref);
+
+	test_check_null_ptr (response->res);
+	test_check_unsigned_eq (response->res_len, 0, NULL);
 
 	http_response_delete (response);
 
@@ -207,6 +294,8 @@ void http_tests_responses (void) {
 
 	(void) printf ("Testing HTTP responses...\n");
 
+	test_http_responses_init ();
+
 	test_http_response_reset ();
 
 	test_http_response_set_status ();
@@ -222,6 +311,8 @@ void http_tests_responses (void) {
 	test_http_response_set_data ();
 
 	test_http_response_set_data_ref ();
+
+	test_http_responses_end ();
 
 	(void) printf ("Done!\n");
 
