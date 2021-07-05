6fbec0d0835382b4e4ef9ca8621b84268d9328aa
ermiry <erick.salas@ermiry.com>
2021-07-05 14:02:58 -0500

    Added HTTP response CORS methods unit tests

--- test/http/responses.c
+++ test/http/responses.c
@@ -5,13 +5,14 @@
 #include <cerver/version.h>
 
 #include <cerver/http/http.h>
+#include <cerver/http/origin.h>
 #include <cerver/http/response.h>
 
 #include "../test.h"
 
 #define TEST_HEADER_LEN		256
 
-static char header[TEST_HEADER_LEN] = { 0 };
+static char json_header[TEST_HEADER_LEN] = { 0 };
 static char header_content_len[TEST_HEADER_LEN] = { 0 };
 static char header_content_type[TEST_HEADER_LEN] = { 0 };
 
@@ -19,12 +20,19 @@ static const char *content_type_header = { "Content-Type: application/json\r\n" 
 static const char *content_length_header = { "Content-Length: 128\r\n" };
 static const char *json_data = { "{ \"oki\": \"doki\" }" };
 
+// cors
+static const char *domain = { "https://ermiry.com" };
+static const char *allow_origin_header = { "Access-Control-Allow-Origin: https://ermiry.com\r\n" };
+static const char *allow_credentials_header = { "Access-Control-Allow-Credentials: true\r\n" };
+static const char *allow_single_method_header = { "Access-Control-Allow-Methods: GET\r\n" };
+static const char *allow_multiple_methods_header = { "Access-Control-Allow-Methods: GET, HEAD, OPTIONS\r\n" };
+
 static HttpCerver *http_cerver = NULL;
 
 static void test_http_responses_init (void) {
 
 	(void) snprintf (
-		header, TEST_HEADER_LEN,
+		json_header, TEST_HEADER_LEN,
 		"HTTP/1.1 200 OK\r\nServer: Cerver/%s\r\nContent-Length: 128\r\nContent-Type: application/json\r\n\r\n",
 		CERVER_VERSION
 	);
@@ -58,6 +66,8 @@ static void test_http_responses_end (void) {
 	http_cerver_delete (http_cerver);
 
 }
+
+#pragma region main
 
 static HttpResponse *test_http_response_new (void) {
 
@@ -125,14 +135,18 @@ static void test_http_response_set_status (void) {
 
 }
 
+#pragma endregion
+
+#pragma region headers
+
 static void test_http_response_set_header (void) {
 
 	HttpResponse *response = test_http_response_new ();
 
-	http_response_set_header (response, strdup (header), strlen (header));
+	http_response_set_header (response, strdup (json_header), strlen (json_header));
 	test_check_ptr (response->header);
-	test_check_str_eq (response->header, header, NULL);
-	test_check_str_len (response->header, strlen (header), NULL);
+	test_check_str_eq (response->header, json_header, NULL);
+	test_check_str_len (response->header, strlen (json_header), NULL);
 
 	http_response_delete (response);
 
@@ -178,8 +192,8 @@ static void test_http_response_add_header (void) {
 	http_response_compile_header (response);
 
 	test_check_ptr (response->header);
-	test_check_str_eq (response->header, header, NULL);
-	test_check_unsigned_eq (response->header_len, strlen (header), NULL);
+	test_check_str_eq (response->header, json_header, NULL);
+	test_check_unsigned_eq (response->header_len, strlen (json_header), NULL);
 
 	test_check_null_ptr (response->data);
 	test_check_unsigned_eq (response->data_len, 0, NULL);
@@ -203,6 +217,7 @@ static void test_http_response_add_content_type_header (void) {
 	test_check_unsigned_eq (result, 0, NULL);
 	test_check_unsigned_eq (response->status, HTTP_STATUS_OK, NULL);
 	test_check_unsigned_eq (response->n_headers, 1, NULL);
+
 	test_check_ptr (response->headers[HTTP_HEADER_CONTENT_TYPE]);
 	test_check_ptr (response->headers[HTTP_HEADER_CONTENT_TYPE]->str);
 	test_check_str_eq (response->headers[HTTP_HEADER_CONTENT_TYPE]->str, content_type_header, NULL);
@@ -239,6 +254,7 @@ static void test_http_response_add_content_length_header (void) {
 	test_check_unsigned_eq (result, 0, NULL);
 	test_check_unsigned_eq (response->status, HTTP_STATUS_OK, NULL);
 	test_check_unsigned_eq (response->n_headers, 1, NULL);
+
 	test_check_ptr (response->headers[HTTP_HEADER_CONTENT_LENGTH]);
 	test_check_ptr (response->headers[HTTP_HEADER_CONTENT_LENGTH]->str);
 	test_check_str_eq (response->headers[HTTP_HEADER_CONTENT_LENGTH]->str, content_length_header, NULL);
@@ -263,6 +279,175 @@ static void test_http_response_add_content_length_header (void) {
 	http_response_delete (response);
 
 }
+
+static void test_http_response_add_json_headers (void) {
+
+	HttpResponse *response = test_http_response_new ();
+
+	http_response_add_json_headers (
+		response, 128
+	);
+
+	test_check_unsigned_eq (response->status, HTTP_STATUS_OK, NULL);
+	test_check_unsigned_eq (response->n_headers, 2, NULL);
+
+	test_check_ptr (response->headers[HTTP_HEADER_CONTENT_TYPE]);
+	test_check_ptr (response->headers[HTTP_HEADER_CONTENT_TYPE]->str);
+	test_check_str_eq (response->headers[HTTP_HEADER_CONTENT_TYPE]->str, content_type_header, NULL);
+	test_check_str_len (response->headers[HTTP_HEADER_CONTENT_TYPE]->str, strlen (content_type_header), NULL);
+	test_check_null_ptr (response->header);
+	test_check_unsigned_eq (response->header_len, 0, NULL);
+
+	test_check_ptr (response->headers[HTTP_HEADER_CONTENT_LENGTH]);
+	test_check_ptr (response->headers[HTTP_HEADER_CONTENT_LENGTH]->str);
+	test_check_str_eq (response->headers[HTTP_HEADER_CONTENT_LENGTH]->str, content_length_header, NULL);
+	test_check_str_len (response->headers[HTTP_HEADER_CONTENT_LENGTH]->str, strlen (content_length_header), NULL);
+	test_check_null_ptr (response->header);
+	test_check_unsigned_eq (response->header_len, 0, NULL);
+
+	// compile header
+	http_response_compile_header (response);
+
+	test_check_ptr (response->header);
+	test_check_str_eq (response->header, json_header, NULL);
+	test_check_unsigned_eq (response->header_len, strlen (json_header), NULL);
+
+	test_check_null_ptr (response->data);
+	test_check_unsigned_eq (response->data_len, 0, NULL);
+	test_check_false (response->data_ref);
+
+	test_check_null_ptr (response->res);
+	test_check_unsigned_eq (response->res_len, 0, NULL);
+
+	http_response_delete (response);
+
+}
+
+#pragma endregion
+
+#pragma region cors
+
+static void test_http_response_add_cors_header (void) {
+
+	HttpResponse *response = test_http_response_new ();
+
+	u8 result = http_response_add_cors_header (
+		response, domain
+	);
+
+	test_check_unsigned_eq (result, 0, NULL);
+	test_check_unsigned_eq (response->status, HTTP_STATUS_OK, NULL);
+	test_check_unsigned_eq (response->n_headers, 1, NULL);
+
+	test_check_ptr (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]);
+	test_check_ptr (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]->str);
+	test_check_str_eq (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]->str, allow_origin_header, NULL);
+	test_check_str_len (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]->str, strlen (allow_origin_header), NULL);
+	test_check_null_ptr (response->header);
+	test_check_unsigned_eq (response->header_len, 0, NULL);
+
+	http_response_delete (response);
+
+}
+
+static void test_http_response_add_cors_header_from_origin (void) {
+
+	HttpResponse *response = test_http_response_new ();
+
+	HttpOrigin origin = { 0 };
+	http_origin_init (&origin, domain);
+
+	u8 result = http_response_add_cors_header_from_origin (
+		response, &origin
+	);
+
+	test_check_unsigned_eq (result, 0, NULL);
+	test_check_unsigned_eq (response->status, HTTP_STATUS_OK, NULL);
+	test_check_unsigned_eq (response->n_headers, 1, NULL);
+
+	test_check_ptr (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]);
+	test_check_ptr (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]->str);
+	test_check_str_eq (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]->str, allow_origin_header, NULL);
+	test_check_str_len (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN]->str, strlen (allow_origin_header), NULL);
+	test_check_null_ptr (response->header);
+	test_check_unsigned_eq (response->header_len, 0, NULL);
+
+	http_response_delete (response);
+
+}
+
+static void test_http_response_add_cors_allow_credentials_header (void) {
+
+	HttpResponse *response = test_http_response_new ();
+
+	u8 result = http_response_add_cors_allow_credentials_header (
+		response
+	);
+
+	test_check_unsigned_eq (result, 0, NULL);
+	test_check_unsigned_eq (response->status, HTTP_STATUS_OK, NULL);
+	test_check_unsigned_eq (response->n_headers, 1, NULL);
+
+	test_check_ptr (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS]);
+	test_check_ptr (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS]->str);
+	test_check_str_eq (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS]->str, allow_credentials_header, NULL);
+	test_check_str_len (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS]->str, strlen (allow_credentials_header), NULL);
+	test_check_null_ptr (response->header);
+	test_check_unsigned_eq (response->header_len, 0, NULL);
+
+	http_response_delete (response);
+
+}
+
+static void test_http_response_add_cors_allow_methods_header_single (void) {
+
+	HttpResponse *response = test_http_response_new ();
+
+	u8 result = http_response_add_cors_allow_methods_header (
+		response, "GET"
+	);
+
+	test_check_unsigned_eq (result, 0, NULL);
+	test_check_unsigned_eq (response->status, HTTP_STATUS_OK, NULL);
+	test_check_unsigned_eq (response->n_headers, 1, NULL);
+
+	test_check_ptr (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_METHODS]);
+	test_check_ptr (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_METHODS]->str);
+	test_check_str_eq (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_METHODS]->str, allow_single_method_header, NULL);
+	test_check_str_len (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_METHODS]->str, strlen (allow_single_method_header), NULL);
+	test_check_null_ptr (response->header);
+	test_check_unsigned_eq (response->header_len, 0, NULL);
+
+	http_response_delete (response);
+
+}
+
+static void test_http_response_add_cors_allow_methods_header_multiple (void) {
+
+	HttpResponse *response = test_http_response_new ();
+
+	u8 result = http_response_add_cors_allow_methods_header (
+		response, "GET, HEAD, OPTIONS"
+	);
+
+	test_check_unsigned_eq (result, 0, NULL);
+	test_check_unsigned_eq (response->status, HTTP_STATUS_OK, NULL);
+	test_check_unsigned_eq (response->n_headers, 1, NULL);
+
+	test_check_ptr (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_METHODS]);
+	test_check_ptr (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_METHODS]->str);
+	test_check_str_eq (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_METHODS]->str, allow_multiple_methods_header, NULL);
+	test_check_str_len (response->headers[HTTP_HEADER_ACCESS_CONTROL_ALLOW_METHODS]->str, strlen (allow_multiple_methods_header), NULL);
+	test_check_null_ptr (response->header);
+	test_check_unsigned_eq (response->header_len, 0, NULL);
+
+	http_response_delete (response);
+
+}
+
+#pragma endregion
+
+#pragma region data
 
 static void test_http_response_set_data (void) {
 
@@ -290,26 +475,34 @@ static void test_http_response_set_data_ref (void) {
 
 }
 
+#pragma endregion
+
 void http_tests_responses (void) {
 
 	(void) printf ("Testing HTTP responses...\n");
 
 	test_http_responses_init ();
 
+	// main
 	test_http_response_reset ();
-
 	test_http_response_set_status ();
 
+	// headers
 	test_http_response_set_header ();
-
 	test_http_response_add_header ();
-
 	test_http_response_add_content_type_header ();
-
 	test_http_response_add_content_length_header ();
+	test_http_response_add_json_headers ();
 
+	// cors
+	test_http_response_add_cors_header ();
+	test_http_response_add_cors_header_from_origin ();
+	test_http_response_add_cors_allow_credentials_header ();
+	test_http_response_add_cors_allow_methods_header_single ();
+	test_http_response_add_cors_allow_methods_header_multiple ();
+
+	// data
 	test_http_response_set_data ();
-
 	test_http_response_set_data_ref ();
 
 	test_http_responses_end ();
