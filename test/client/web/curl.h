008e6c126ef12fd3dd555c9aaed1722973021475
ermiry <erick.salas@ermiry.com>
2021-08-07 21:23:43 -0500

    Updated test curl methods to take an expected status

--- test/client/web/curl.h
+++ test/client/web/curl.h
@@ -5,6 +5,8 @@
 
 #include <curl/curl.h>
 
+#include <cerver/http/status.h>
+
 typedef enum CurlResult {
 
 	CURL_RESULT_NONE			= 0,
@@ -21,76 +23,85 @@ typedef size_t (*curl_write_data_cb)(
 // creates an destroy a local CURL structure
 // returns 0 on success, 1 on any error
 extern unsigned int curl_simple_full (
-	const char *address
+	const char *address,
+	const http_status expected_status
 );
 
 // performs a simple curl request to the specified address
 // uses an already created CURL structure
 // returns 0 on success, 1 on any error
-extern unsigned int curl_simple (
-	CURL *curl, const char *address
+extern CurlResult curl_simple (
+	CURL *curl, const char *address,
+	const http_status expected_status
 );
 
 // works like curl_simple () but adds a custom auth header
 // returns 0 on success, 1 on any error
-extern unsigned int curl_simple_with_auth (
+extern CurlResult curl_simple_with_auth (
 	CURL *curl, const char *address,
+	const http_status expected_status,
 	const char *authorization
 );
 
 // performs a simple curl request to the specified address
 // uses an already created CURL structure
 // returns 0 on success, 1 on any error
-extern unsigned int curl_simple_handle_data (
+extern CurlResult curl_simple_handle_data (
 	CURL *curl, const char *address,
+	const http_status expected_status,
 	curl_write_data_cb write_cb, char *buffer
 );
 
 // performs a POST with application/x-www-form-urlencoded data
 // uses an already created CURL structure
 // returns 0 on success, 1 on any error
-extern unsigned int curl_simple_post (
+extern CurlResult curl_simple_post (
 	CURL *curl, const char *address,
+	const http_status expected_status,
 	const char *data, const size_t datalen
 );
 
 // works like curl_simple_post ()
 // but sets a custom Authorization header
-extern unsigned int curl_simple_post_with_auth (
+extern CurlResult curl_simple_post_with_auth (
 	CURL *curl, const char *address,
+	const http_status expected_status,
 	const char *data, const size_t datalen,
 	const char *authorization
 );
 
-// works as curl_simple_post ()
+// works like curl_simple_post ()
 // but has the option to handle the result data
-extern unsigned int curl_simple_post_handle_data (
+extern CurlResult curl_simple_post_handle_data (
 	CURL *curl, const char *address,
+	const http_status expected_status,
 	const char *data, const size_t datalen,
 	curl_write_data_cb write_cb, char *buffer
 );
 
 // performs a multi-part request with just one value
 // returns 0 on success, 1 on error
-extern unsigned int curl_post_form_value (
+extern CurlResult curl_post_form_value (
 	CURL *curl, const char *address,
+	const http_status expected_status,
 	curl_write_data_cb write_cb, char *buffer,
-	const char *key, const char *value,
-	const unsigned int expected_status
+	const char *key, const char *value
 );
 
 // uploads a file to the requested route performing a multi-part request
 // returns 0 on success, 1 on error
-extern unsigned int curl_upload_file (
+extern CurlResult curl_upload_file (
 	CURL *curl, const char *address,
+	const http_status expected_status,
 	curl_write_data_cb write_cb, char *buffer,
 	const char *filename
 );
 
 // uploads two files in the same multi-part request
 // returns 0 on success, 1 on error
-extern unsigned int curl_upload_two_files (
+extern CurlResult curl_upload_two_files (
 	CURL *curl, const char *address,
+	const http_status expected_status,
 	curl_write_data_cb write_cb, char *buffer,
 	const char *filename_one, const char *filename_two
 );
@@ -98,8 +109,9 @@ extern unsigned int curl_upload_two_files (
 // uploads a file to the requested route performing a multi-part request
 // and also adds another value to the request
 // returns 0 on success, 1 on error
-extern unsigned int curl_upload_file_with_extra_value (
+extern CurlResult curl_upload_file_with_extra_value (
 	CURL *curl, const char *address,
+	const http_status expected_status,
 	const char *filename,
 	const char *key, const char *value
 );
