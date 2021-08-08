d6f3f061242b7390baae42358671de30ff3a4cea
ermiry <erick.salas@ermiry.com>
2021-08-07 21:30:50 -0500

    Removed extra logs from web cerver integration tests

--- test/web/admin.c
+++ test/web/admin.c
@@ -76,12 +76,7 @@ int main (int argc, char **argv) {
 
 	cerver_init ();
 
-	printf ("\n");
 	cerver_version_print_full ();
-	printf ("\n");
-
-	cerver_log_debug ("Simple Web Cerver Example");
-	printf ("\n");
 
 	web_cerver = cerver_create (
 		CERVER_TYPE_WEB,
