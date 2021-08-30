0b8a7811c1f5bd713ac3943165537ad7eb4221e8
ermiry <erick.salas@ermiry.com>
2021-08-30 04:56:42 -0500

    Updated version to Beta 2.0b-49

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-ARG CERVER_VERSION=2.0b-48
+ARG CERVER_VERSION=2.0b-49
 
 FROM gcc as builder
 
--- cerver-install.sh
+++ cerver-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CERVER_VERSION=2.0b-48
+CERVER_VERSION=2.0b-49
 
 # sudo apt-get update && sudo apt-get install -y make wget
 
