130c7e13f4718d8aae59edee9a89009e08540310
ermiry <erick.salas@ermiry.com>
2021-10-13 23:03:12 -0500

    Updated dedicated run script with latest unit tests

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./test/bin/types || { exit 1; }
+
 ./test/bin/cerver/test || { exit 1; }
 
 ./test/bin/client/test || { exit 1; }
@@ -10,7 +12,11 @@
 
 ./test/bin/collections --quiet || { exit 1; }
 
+./test/bin/files || { exit 1; }
+
 ./test/bin/packets || { exit 1; }
+
+./test/bin/receive || { exit 1; }
 
 ./test/bin/system || { exit 1; }
 
